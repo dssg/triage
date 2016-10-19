f9377672d6ebbd1a4f0f1ea82ff12f40cc5a2df2
Eric Potash <eric@k2co3.net>
2016-10-19 20:03:00 +0000

    PEP8

--- collate/collate.py
+++ collate/collate.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from itertools import product, chain
 
+
 def make_list(a):
         return [a] if not type(a) in (list, tuple) else list(a)
+
 
 class Select(object):
     """
@@ -14,7 +16,7 @@ class Select(object):
             columns: collection of SQL column strings
             table: source table or SQL query from which to select
             where: SQL where clause
-            groupby: SQL group-by clause 
+            groupby: SQL group-by clause
         """
         self.columns = columns
         self.table = table
@@ -26,8 +28,10 @@ class Select(object):
         return ("SELECT {columns}\n"
                 "FROM {table}\n"
                 "WHERE {where}\n"
-                "GROUP BY {groupby}".format(columns=columns, table=self.table, 
-                       where=self.where, groupby=self.groupby))
+                "GROUP BY {groupby}".format(
+                    columns=columns, table=self.table,
+                    where=self.where, groupby=self.groupby))
+
 
 class Aggregate(object):
     """
@@ -41,7 +45,7 @@ class Aggregate(object):
             name: a name for the quantity, usd in the aggregate column name
 
         Note that quantity and function can also be collections of the above,
-        in which case the cross product of those is used. If quantity is a 
+        in which case the cross product of those is used. If quantity is a
         collection than name should also be a collection of the same length.
         """
         self.quantities = make_list(quantity)
@@ -50,14 +54,15 @@ class Aggregate(object):
         if name is not None:
             self.quantity_names = make_list(name)
             if len(self.quantity_names) != len(self.quantities):
-                raise ValueError("Names length doesn't match quantities length")
+                raise ValueError("Name length doesn't match quantitie length")
         else:
             self.quantity_names = map(str, self.quantities)
 
     def get_sql(self, when=None, prefix=None):
         """
         Args:
-            when: used in a case statement to filter the rows going into the aggregation function
+            when: used in a case statement to filter the rows going into the
+                aggregation function
             prefix: prefix for column names
         """
         if prefix is None:
@@ -67,23 +72,31 @@ class Aggregate(object):
         if when is None:
             quantity_template = "{function}({quantity})"
         else:
-            quantity_template = "{function}(CASE WHEN {when} THEN {quantity} END)"
+            quantity_template = ("{function}(CASE WHEN {when} "
+                                 "THEN {quantity} END)")
+
         template = "%s AS %s" % (quantity_template, name_template)
 
-        args = product(self.functions, zip(self.quantities, self.quantity_names))
-        return [template.format(function=function, quantity=quantity, 
-            when=when, quantity_name=quantity_name, prefix=prefix) 
+        args = product(self.functions, zip(self.quantities,
+                                           self.quantity_names))
+        return [template.format(function=function, quantity=quantity,
+                                when=when, quantity_name=quantity_name,
+                                prefix=prefix)
                 for function, (quantity, quantity_name) in args]
 
+
 class SpacetimeAggregation(object):
-    def __init__(self, aggregates, intervals, table, groupby, dates, prefix=None, date_column=None):
+    def __init__(self, aggregates, intervals, table, groupby, dates,
+                 prefix=None, date_column=None):
         """
         Args:
             aggregates: collection of Aggregate objects
-            intervals: collection of PostgreSQL time interval strings, e.g. ["1 month', "1 year"]
+            intervals: collection of PostgreSQL time interval strings,
+                e.g. ["1 month', "1 year"]
             table: name of table (or SQL subquery) to select from
             groupby: SQL group by clause
-            dates: list of PostgreSQL date strings, e.g. ["2012-01-01", "2013-01-01"]
+            dates: list of PostgreSQL date strings,
+                e.g. ["2012-01-01", "2013-01-01"]
             prefix: name of prefix for column names, defaults to table
             date_column: name of date column in the table, defaults to "date"
         """
@@ -107,10 +120,11 @@ class SpacetimeAggregation(object):
         """
         when = "'{date}' - {date_column} < interval '{interval}'".format(
                 interval=interval, date=date, date_column=self.date_column)
-        prefix = "{prefix}_{groupby}_{interval}_".format(prefix=self.prefix,
-                interval=interval.replace(' ', ''), groupby=self.groupby)
+        prefix = "{prefix}_{groupby}_{interval}_".format(
+                prefix=self.prefix, interval=interval.replace(' ', ''),
+                groupby=self.groupby)
+
         return chain(*(a.get_sql(when, prefix) for a in self.aggregates))
-            
 
     def get_queries(self):
         """
@@ -121,8 +135,10 @@ class SpacetimeAggregation(object):
         queries = []
 
         for date in self.dates:
-            columns = chain(*(self._get_aggregates_sql(i, date) for i in self.intervals))
-            where = "{date_column} < '{date}'".format(date_column=self.date_column, date=date)
+            columns = chain(*(
+                    self._get_aggregates_sql(i, date) for i in self.intervals))
+            where = "{date_column} < '{date}'".format(
+                    date_column=self.date_column, date=date)
             queries.append(Select(columns, self.table, where, self.groupby))
 
         return queries
