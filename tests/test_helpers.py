e3f52a5d2b24f94bd1d52aa8a0cfff714ad1e461
Matt Bauman <mbauman@gmail.com>
2016-12-29 13:44:04 -0500

    support lists of tuples in quantities

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,7 +2,8 @@ from collate import collate
 
 def assert_contains(haystack, needle):
     for h in haystack:
-        if needle in h: return
+        if type(h) is tuple and needle in h[0]: return
+        elif needle in h: return
     assert False
 
 def test_multicompare_lists():
@@ -13,7 +14,7 @@ def test_multicompare_lists():
     assert_contains(d.values(), "col = 'a'")
     assert_contains(d.values(), "col = 'b'")
     assert_contains(d.values(), "col = 'c'")
-    assert_contains(map(str.lower, d.values()), "col is null")
+    assert_contains(map(lambda x: x[0].lower(), d.values()), "col is null")
 
     d = collate.MultiCompare('col','>',[1,2,3],[]).quantities
     assert len(d) == 4
@@ -22,7 +23,7 @@ def test_multicompare_lists():
     assert_contains(d.values(), "col > 1")
     assert_contains(d.values(), "col > 2")
     assert_contains(d.values(), "col > 3")
-    assert_contains(map(str.lower, d.values()), "col is null")
+    assert_contains(map(lambda x: x[0].lower(), d.values()), "col is null")
 
     d = collate.MultiCompare('col','=',['a','b','c'], [], include_null=False).quantities
     assert len(d) == 3
@@ -45,7 +46,7 @@ def test_multicompare_lists():
     assert_contains(d.values(), "really_long_column_name = 'really long string value that is like others'")
     assert_contains(d.values(), "really_long_column_name = 'really long string value that is quite alike to others'")
     assert_contains(d.values(), "really_long_column_name = 'really long string value that is also like everything else'")
-    assert_contains(map(str.lower, d.values()), "really_long_column_name is null")
+    assert_contains(map(lambda x: x[0].lower(), d.values()), "really_long_column_name is null")
 
 def test_multicompare_dicts():
     d = collate.MultiCompare('col','=',{'vala': 'a','valb': 'b','valc': 'c'}, []).quantities
@@ -59,7 +60,7 @@ def test_multicompare_dicts():
     assert_contains(d.keys(), 'valb')
     assert_contains(d.keys(), 'valc')
     assert_contains(map(str.lower, d.keys()), 'null')
-    assert_contains(map(str.lower, d.values()), "col is null")
+    assert_contains(map(lambda x: x[0].lower(), d.values()), "col is null")
 
     d = collate.MultiCompare('col','<',{'val1': 1,'val2': 2,'val3': 3}, []).quantities
     assert len(d) == 4
@@ -72,7 +73,7 @@ def test_multicompare_dicts():
     assert_contains(d.keys(), 'val2')
     assert_contains(d.keys(), 'val3')
     assert_contains(map(str.lower, d.keys()), 'null')
-    assert_contains(map(str.lower, d.values()), "col is null")
+    assert_contains(map(lambda x: x[0].lower(), d.values()), "col is null")
 
     d = collate.MultiCompare('long_column_name','=',
         {'really long string key that is similar to others': 'really long string value that is similar to others',
@@ -88,4 +89,4 @@ def test_multicompare_dicts():
     assert_contains(d.values(), "long_column_name = 'really long string value that is like others'")
     assert_contains(d.values(), "long_column_name = 'really long string value that is quite alike to others'")
     assert_contains(d.values(), "long_column_name = 'really long string value that is also like everything else'")
-    assert_contains(map(str.lower, d.values()), "long_column_name is null")
+    assert_contains(map(lambda x: x[0].lower(), d.values()), "long_column_name is null")
