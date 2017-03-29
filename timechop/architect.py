1a25c6abbae76d680d68d4c16347daf3cee5a2d1
Erika Salomon <ecsalomon@gmail.com>
2017-03-29 15:11:52 +0000

    turn on overwrite, remove write_index

--- timechop/architect.py
+++ timechop/architect.py
@@ -156,11 +156,11 @@ class Architect(object):
 
         # store the matrix
         metta.archive_matrix(
-            matrix_metadata,
-            matrix_filename,
-            directory = self.matrix_directory,
-            format = 'csv',
-            write_index = False
+            matrix_config=matrix_metadata,
+            df_matrix=matrix_filename,
+            overwrite=True,
+            directory=self.matrix_directory,
+            format='csv'
         )
 
         # clean up files and database before finishing
