8ef119477b64c9ba15f5c8804ba0aceadb481e72
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-04-06 18:46:34 -0500

    Add leave-one-in feature group mixer strategy [Resolves #88] (#89)
    - Rename 'all' feature group mixer strategy to the more accurate leave-one-in
    - Create new 'all' feature group mixer strategy that smashes all groups into one feature dictionary
    - Improve message of exception that is thrown when an unknown strategy is given
    - Beef up docstrings

--- tests/test_feature_group_mixer.py
+++ tests/test_feature_group_mixer.py
@@ -16,3 +16,31 @@ def test_feature_group_mixer_leave_one_out():
         dict(itertools.chain(english_numbers.items(), letters.items())),
     ]
     assert result == expected
+
+
+def test_feature_group_mixer_leave_one_in():
+    english_numbers = {'one': ['two', 'three'], 'four': ['five', 'six']}
+    letters = {'a': ['b', 'c'], 'd': ['e', 'f']}
+    german_numbers = {'eins': ['zwei', 'drei'], 'vier': ['funf', 'sechs']}
+    feature_groups = [english_numbers, letters, german_numbers]
+
+    result = FeatureGroupMixer(['leave-one-in']).generate(feature_groups)
+    expected = [
+        english_numbers,
+        letters,
+        german_numbers
+    ]
+    assert result == expected
+
+
+def test_feature_group_mixer_all():
+    english_numbers = {'one': ['two', 'three'], 'four': ['five', 'six']}
+    letters = {'a': ['b', 'c'], 'd': ['e', 'f']}
+    german_numbers = {'eins': ['zwei', 'drei'], 'vier': ['funf', 'sechs']}
+    feature_groups = [english_numbers, letters, german_numbers]
+
+    result = FeatureGroupMixer(['all']).generate(feature_groups)
+    expected = [
+        dict(itertools.chain(english_numbers.items(), letters.items(), german_numbers.items())),
+    ]
+    assert result == expected
