b9648fe8f87df4d410679a31e50fb20a9e00d5ac
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-04-20 16:21:26 -0500

    Support categoricals on array columns [Resolves #121]
    - Parse array_categoricals as an aggregation, use Compare with array casting for choices
    - Make sure collate master is installed

--- tests/test_feature_generators.py
+++ tests/test_feature_generators.py
@@ -183,6 +183,100 @@ def test_dynamic_categoricals():
             assert records == expected_output[output_table]
 
 
+def test_array_categoricals():
+    aggregate_config = [{
+        'prefix': 'aprefix',
+        'array_categoricals': [
+            {
+                'column': 'cat_one',
+                'choices': ['good', 'bad', 'inbetween'],
+                'metrics': ['sum']
+            },
+        ],
+        'groups': ['entity_id'],
+        'intervals': ['all'],
+        'knowledge_date_column': 'knowledge_date',
+        'from_obj': 'data'
+    }]
+    expected_output = {
+        'aprefix_entity_id': [
+            {
+                'entity_id': 3,
+                'as_of_date': date(2013, 9, 30),
+                'aprefix_entity_id_all_cat_one_good_sum': 0,
+                'aprefix_entity_id_all_cat_one_inbetween_sum': 0,
+                'aprefix_entity_id_all_cat_one_bad_sum': 1
+            },
+            {
+                'entity_id': 1,
+                'as_of_date': date(2014, 9, 30),
+                'aprefix_entity_id_all_cat_one_good_sum': 1,
+                'aprefix_entity_id_all_cat_one_inbetween_sum': 0,
+                'aprefix_entity_id_all_cat_one_bad_sum': 0
+            },
+            {
+                'entity_id': 3,
+                'as_of_date': date(2014, 9, 30),
+                'aprefix_entity_id_all_cat_one_good_sum': 0,
+                'aprefix_entity_id_all_cat_one_inbetween_sum': 0,
+                'aprefix_entity_id_all_cat_one_bad_sum': 1
+            },
+            {
+                'entity_id': 4,
+                'as_of_date': date(2014, 9, 30),
+                'aprefix_entity_id_all_cat_one_good_sum': 0,
+                'aprefix_entity_id_all_cat_one_inbetween_sum': 0,
+                'aprefix_entity_id_all_cat_one_bad_sum': 1
+            },
+        ]
+
+    }
+
+    with testing.postgresql.Postgresql() as postgresql:
+        engine = create_engine(postgresql.url())
+        input_data = [
+            # entity_id, knowledge_date, cat_one, quantity_one
+            (1, date(2014, 1, 1), ['good', 'good'], 10000),
+            (1, date(2014, 10, 11), ['good'], None),
+            (3, date(2012, 6, 8), ['bad'], 342),
+            (3, date(2014, 12, 21), ['inbetween'], 600),
+            (4, date(2014, 4, 4), ['bad'], 1236)
+        ]
+
+        engine.execute("""
+            create table data (
+                entity_id int,
+                knowledge_date date,
+                cat_one varchar[],
+                quantity_one float
+            )
+        """)
+        for row in input_data:
+            engine.execute(
+                'insert into data values (%s, %s, %s, %s)',
+                row
+            )
+
+        features_schema_name = 'features'
+
+        output_tables = FeatureGenerator(
+            db_engine=engine,
+            features_schema_name=features_schema_name
+        ).create_all_tables(
+            feature_dates=['2013-09-30', '2014-09-30'],
+            feature_aggregation_config=aggregate_config,
+        )
+
+        for output_table in output_tables:
+            records = pandas.read_sql(
+                'select * from {}.{} order by as_of_date, entity_id'.format(
+                    features_schema_name, output_table
+                ),
+                engine
+            ).to_dict('records')
+            assert records == expected_output[output_table]
+
+
 def test_generate_table_tasks():
     aggregate_config = [{
         'prefix': 'prefix1',
