73b5e3c5900d91e2b10f488e65b74c41a8a63995
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-05-15 11:44:38 -0500

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('requirements_dev.txt') as requirements_dev_file:
 
 setup(
     name='results_schema',
-    version='1.0.2',
+    version='1.0.3',
     description="Store results of modeling runs",
     long_description=readme,
     author="Center for Data Science and Public Policy",
