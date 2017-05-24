4df86948ce7ed1abddfafa6faab40a062ba53939
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-05-24 15:12:54 -0500

    Initial cookiecutter run

---
+++ README.md
@@ -0,0 +1,7 @@
+#The Architect 
+
+Plan, design, and build train and test matrices
+
+[![Build Status](https://travis-ci.org/dssg/architect.svg?branch=master)](https://travis-ci.org/dssg/architect)
+[![codecov](https://codecov.io/gh/dssg/architect/branch/master/graph/badge.svg)](https://codecov.io/gh/dssg/architect)
+[![codeclimate](https://codeclimate.com/github/dssg/architect.png)](https://codeclimate.com/github/dssg/architect)
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('LICENSE') as license_file:
+    license = license_file.read()
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.readlines()
+
+with open('requirements_dev.txt') as dev_requirements_file:
+    test_requirements = dev_requirements_file.readlines()
+
+
+setup(
+    name='architect',
+    version='0.1.0',
+    description="Plan, design, and build train and test matrices",
+    long_description=readme,
+    author="Center for Data Science and Public Policy",
+    author_email='datascifellows@gmail.com',
+    url='https://github.com/dssg/architect',
+    packages=[
+        'architect',
+    ],
+    include_package_data=True,
+    install_requires=requirements,
+    license=license,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
