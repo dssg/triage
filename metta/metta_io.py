ff5d2c629103bb60e17b3ed7bf7295ea7fcf2716
Avishek Kumar <avishek.kumar@outlook.com>
2017-08-22 11:42:57 -0500

    fixed issue with date parsing (#48)
    * fixed issue with date parsing

--- metta/metta_io.py
+++ metta/metta_io.py
@@ -198,10 +198,10 @@ def _store_matrix(metadata, df_data, title, directory, format='hd5'):
     if format == 'hd5':
 
         for col in df_data.columns:
-            if isinstance(df_data[col].dtype, object):
-                df_data[col] = df_data[col].astype(float)
-            elif (df_data[col].dtype == np.dtype('datetime64[ns]')):
+            if (df_data[col].dtype == np.dtype('datetime64[ns]')):
                 df_data[col] = df_data[col].map(lambda x: x.timestamp())
+            elif isinstance(df_data[col].dtype, object):
+                df_data[col] = df_data[col].astype(float)
 
         hdf = pd.HDFStore(directory + '/' + title + '.h5',
                           mode='w',
