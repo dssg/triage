7ca464263b248597e03454adddea70b3b8e390a1
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-10-23 16:42:23 -0500

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('requirements_dev.txt') as dev_requirements_file:
 
 setup(
     name='timechop',
-    version='0.1.4',
+    version='0.1.5',
     description="Implements temporal validation for machine learning/",
     long_description=readme,
     author="Center for Data Science and Public Policy",
