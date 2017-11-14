5c7b6c8d7034f0bea49fdb574317d4b74b16c9cb
Jesse London <jesselondon@gmail.com>
2017-11-14 15:59:15 -0600

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('requirements_dev.txt') as dev_requirements_file:
 
 setup(
     name='matrix-architect',
-    version='1.0.0',
+    version='1.0.1',
     description="Plan, design, and build train and test matrices",
     long_description=readme,
     author="Center for Data Science and Public Policy",
