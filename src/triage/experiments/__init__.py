a1316f6f0445295475b74a1729d1c7171a9b2993
Jesse London <jesselondon@gmail.com>
2017-11-29 14:09:09 -0600

    to understand recursion, first you must understand recursion

--- src/triage/experiments/__init__.py
+++ src/triage/experiments/__init__.py
@@ -1,8 +1,8 @@
+CONFIG_VERSION = 'v3'
+
 from .base import ExperimentBase
 from .multicore import MultiCoreExperiment
 from .singlethreaded import SingleThreadedExperiment
-
-CONFIG_VERSION = 'v3'
 
 __all__ = (
     'ExperimentBase',
