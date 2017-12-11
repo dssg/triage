c1931124db6aeeca4fdd6dcb2005e70268e07e74
Jesse London <jesselondon@gmail.com>
2017-12-11 14:52:34 -0600

    removed files made superfluous by triage and moved library modules into place

--- metta/datafiles.py
+++
@@ -1,6 +0,0 @@
-from pkg_resources import resource_filename
-
-example_directory = resource_filename(__name__, '/data/')
-example_data_csv = resource_filename(__name__, '/data/titanic.csv')
-example_data_h5 = resource_filename(__name__, '/data/titanic.h5')
-example_aws_creds = resource_filename(__name__, '/data/rootkey.yaml')
