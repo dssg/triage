9fa31f0c19ed48a5e922864b751a3aa83590095b
Avishek Kumar <avishek.kumar@outlook.com>
2018-02-03 00:18:00 +0000

    Allow passing of label generation query #331

--- src/triage/component/architect/label_generators.py
+++ src/triage/component/architect/label_generators.py
@@ -1,4 +1,5 @@
 import logging
+from abc import ABCMeta, abstractmethod
 
 from triage.component.architect.validations import (
     table_should_have_data,
@@ -8,17 +9,40 @@ from triage.component.architect.validations import (
 )
 
 
-class BinaryLabelGenerator(object):
-
+class BinaryLabelBase(object, metaclass=ABCMeta):
     def __init__(self, events_table, db_engine):
         self.events_table = events_table
         self.db_engine = db_engine
 
     def validate(self):
         table_should_have_data(self.events_table, self.db_engine)
-        column_should_be_intlike(self.events_table, 'entity_id', self.db_engine)
-        column_should_be_timelike(self.events_table, 'outcome_date', self.db_engine)
-        column_should_be_booleanlike(self.events_table, 'outcome', self.db_engine)
+        column_should_be_intlike(
+            self.events_table, 'entity_id', self.db_engine)
+        column_should_be_timelike(
+            self.events_table, 'outcome_date', self.db_engine)
+        column_should_be_booleanlike(
+            self.events_table, 'outcome', self.db_engine)
+
+    def _create_labels_table(self, labels_table_name):
+        self.db_engine.execute(
+            'drop table if exists {}'.format(labels_table_name)
+        )
+        self.db_engine.execute('''
+            create table {} (
+            entity_id int,
+            as_of_date date,
+            label_timespan varchar(30),
+            label_name varchar(30),
+            label_type varchar(30),
+            label int
+        )'''.format(labels_table_name))
+
+    @abstractmethod
+    def generate(self):
+        pass
+
+
+class BinaryLabelGenerator(BinaryLabelBase):
 
     def generate(
         self,
@@ -44,6 +68,7 @@ class BinaryLabelGenerator(object):
             start_date=start_date,
             label_timespan=label_timespan,
         )
+
         logging.debug('Running label generation query: %s', query)
         self.db_engine.execute(query)
         return labels_table
@@ -60,7 +85,7 @@ class BinaryLabelGenerator(object):
             label_name varchar(30),
             label_type varchar(30),
             label int
-        )'''.format(labels_table_name))
+        );'''.format(labels_table_name))
 
     def generate_all_labels(
         self,
@@ -83,9 +108,35 @@ class BinaryLabelGenerator(object):
                 )
         nrows = [
             row[0] for row in
-            self.db_engine.execute('select count(*) from {}'.format(labels_table))
+            self.db_engine.execute(
+                'select count(*) from {}'.format(labels_table))
         ][0]
         if nrows == 0:
-            logging.warning('Done creating labels, but no rows in labels table!')
+            logging.warning(
+                'Done creating labels, but no rows in labels table!')
         else:
             logging.info('Rows in labels table: %s', nrows)
+
+
+class QueryBinaryLabelGenerator(BinaryLabelBase):
+    def generate(self, query, start_date, label_timespan, labels_table):
+        """Generate labels table using a query
+
+        Parameters
+        ----------
+        query: str
+            query to generate labels table
+        start_date: str
+            as of date
+        label_timespan: str
+            postgresql readable time interval
+        labels_table: str
+            name of labels table
+        """
+        self._create_labels_table(labels_table)
+        insert_query = "insert into {labels_table} ".format(
+            labels_table=labels_table)
+
+        logging.debug("Running label creation query")
+        logging.debug(insert_query+query)
+        self.db_engine.execute(insert_query+query)
