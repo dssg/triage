94d6f3553b0bf2d82d1ee204b6ba62815182d6f5
ivanhigueram <ivanhigueram@gmail.com>
2018-08-01 02:03:46 +0000

    Add new functions to evaluator and start nb
    This commit adds new functionalities to the Model python class and
    creates a notebook to start the tutorial. This ipynb explains how to
    call models from the SQL database, and how to interact with the class
    objects (its attributes and methods).
    
    To-do:
     - Comparison between models (jaccard and rank correlation)
     - More on same model assessment (scores distributions)

--- src/triage/component/postmodeling/contrast/evaluator.py
+++ src/triage/component/postmodeling/contrast/evaluator.py
@@ -14,6 +14,9 @@ To run most of this routines you will need:
 
 import pandas as pd
 from sqlalchemy.sql import text
+from matplotlib import pyplot as plt
+from sklearn import metrics
+
 from collections import namedtuple
 from utils.file_helpers import download_s3
 from utils.test_conn import db_engine
@@ -22,14 +25,15 @@ from utils.test_conn import db_engine
 
 
 def get_models_ids(audited_model_group_ids):
-    exec=db_engine.execute(text("""
+    query = db_engine.execute(text("""
     SELECT model_group_id,
            model_id
     FROM model_metadata.models
     WHERE model_group_id = ANY(:ids);
     """), ids=audited_model_group_ids)
 
-    l = [i for i in exec]
+    ModelTuple = namedtuple('Model', ['model_group_id', 'model_id'])
+    l = [ModelTuple(*i) for i in query]
 
     return(l)
 
@@ -85,19 +89,162 @@ class ModelExtractor(object):
         USING(model_id);
         """.format(self.model_group_id, self.model_id),
                    con=db_engine)
-     
+
         # Add metadata attributes to model
+        self.model_type = model_metadata.loc[0, 'model_type']
         self.hyperparameters = model_metadata.loc[0, 'hyperparameters']
         self.model_hash = model_metadata.loc[0, 'model_hash']
         self.train_matrix_uuid = model_metadata.loc[0, 'train_matrix_uuid']
-        self.matrix_uuid = model_metadata.loc[0, 'matrix_uuid']
+        self.pred_matrix_uuid = model_metadata.loc[0, 'matrix_uuid']
+        self.train_matrix = None
+        self.pred_matrix = None
+        self.preds = None
+        self.feature_importances = None
 
-   # def load_matrices(self, path):
+    def _load_predictions(self):
+        preds = pd.read_sql("""
+                SELECT model_id,
+                       entity_id,
+                       as_of_date,
+                       score,
+                       label_value,
+                       rank_abs,
+                       rank_pct,
+                       test_label_timespan
+                FROM test_results.predictions
+                WHERE model_id = {}
+                AND label_value IS NOT NULL
+                """.format(self.model_id),
+                con=db_engine)
+        self.preds = preds
 
-   #    if 's3' in path:
-   #        self.train_matrix = download_s3(str(path + self.train_matrix_uuid))
-   #        self.test_matrix = download_s3(str(path + self.matrix_uuid))
-   #    else:
-   #        self.train_matrix = pd.read_csv(str(path + self.train_matrix_uuid + '.csv'))
-   #        self.test_matrix = pd.read_csv(str(path + self.matrix_uuid + '.csv'))
+    def _load_feature_importances(self):
+        features = pd.read_sql("""
+               SELECT model_id,
+                      entity_id,
+                      as_of_date,
+                      feature,
+                      method,
+                      feature_value,
+                      importance_score
+               FROM test_results.individual_importances
+               WHERE model_id = {}
+               """.format(self.model_id),
+               con=db_engine)
+        self.feature_importances = features
+
+    def _load_metrics(self):
+        metrics = pd.read_sql("""
+                SELECT model_id,
+                       metric,
+                       metric_parameter,
+                       value,
+                       num_labeled_examples,
+                       num_labeled_above_treshold,
+                       num_positive_labels
+                FROM test_results.evaluation
+                WHERE model_id = {}
+                """.format(self.model_id),
+                con=db_engine)
+
+    def _fetch_matrix(self, matrix_hash, path):
+        '''
+        Load matrices into object (Beware!)
+        This can be a memory intensive process. This function will use the
+        stored model parameters to load matrices from a .csv file. If the path
+        is a s3 path, the method will use s3fs to open it. If the path is a local
+        machine path, it will be opened as a pandas dataframe.
+
+        Arguments:
+            - PATH <s3 path or local path>
+        '''
+        if 's3' in path:
+            mat = download_s3(str(path + self.train_matrix_uuid))
+        else:
+            mat = pd.read_csv(str(path + self.train_matrix_uuid + '.csv'))
+
+        return(mat)
+
+    def load_features_preds_matrix(self, *path):
+        '''
+        Load predicion matrix (from s3 or system file) and merge with
+        label values from the test_results.predictions tables. The outcome
+        is a pandas dataframe with a matrix for each entity_id, its predicted
+        label, scores, and the feature matrix. This last object will be store
+        as the pred_matrix attribute of the class.
+
+        Arguments:
+            - Arguments inherited from _fetch_matrices: 
+                - path: relative path to the triage matrices folder or s3 path
+        '''
+
+        if self.train_matrix is None:
+            mat = self._fetch_matrix(self.pred_matrix_uuid, *path)
+
+        if self.preds is None:
+            self._load_predictions()
+
+        if self.feature_importances is None:
+            self._load_feature_importances()
+
+        if self.pred_matrix is None:
+            # Merge feature/prediction matrix with predictions
+            merged_df = mat.merge(self.preds,
+                                  on='entity_id',
+                                  how='inner',
+                                  suffixes=('test', 'pred'))
+
+            self.pred_matrix = merged_df
+
+    def load_train_matrix(self, *path):
+        '''
+        Load training metrix (from s3 or system file). This object will be store 
+        as the train_matrix object of the class.
+
+        Arguments:
+            - Arguments inherited from _fecth_matrices:
+                - path: relative path to the triage matrices folder or s3 path
+        '''
+        if self.train_matrix is None:
+            self.train_matrix = self._fetch_matrix(self.train_matrix_uuid, *path)
+
+#    def plot_score_label_distributions(self):
+
+    def plot_feature_importances(self, n_features=30, figsize=(16, 12)):
+        """Generate a bar chart of the top n feature importances (by absolute value)
+        Arguments:
+           n_features (int) -- number of top features to plot
+           figsize (tuple) -- figure size to pass to matplotlib
+        """
+        # TODO: allow more of the figure arguments to be passed to the method
+
+        if self.feature_importances is None:
+            self._load_feature_importances()
+
+        humanized_featnames = self.feature_importances['feature']
+        feature_importances = self.feature_importances['feature_value']
+
+        # TODO: refactor to just make this a slice of self.features
+        importances = list(zip(humanized_featnames, list(feature_importances)))
+        importances = pd.DataFrame(importances, columns=['Feature', 'Score'])
+        importances = importances.set_index('Feature')
+
+        # Sort by the absolute value of the importance of the feature
+        importances['sort'] = abs(importances['Score'])
+        importances = importances.sort_values(by='sort', ascending=False).drop('sort', axis=1)
+        importances = importances[0:n_features]
+ 
+        # Show the most important positive feature at the top of the graph
+        importances = importances.sort_values(by='Score', ascending=True)
+
+        fig, ax = plt.subplots(figsize=figsize)
+        ax.tick_params(labelsize=16)
+        importances.plot(kind="barh", legend=False, ax=ax)
+        ax.set_frame_on(False)
+        ax.set_xlabel('Score', fontsize=20)
+        ax.set_ylabel('Feature', fontsize=20)
+        plt.tight_layout()
+        plt.title('Top Feature Importances', fontsize=20).set_position([.5, 0.99])
+
+ 
 
