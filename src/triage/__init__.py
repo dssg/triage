9239b8bb5002897350c952600f1377bb22628f61
Tristan Crockett <tristan.h.crockett@gmail.com>
2018-10-05 12:00:35 -0500

    Bump version in code

--- src/triage/__init__.py
+++ src/triage/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Center for Data Science and Public Policy"""
 __email__ = 'datascifellows@gmail.com'
-__version__ = '2.2.0'
+__version__ = '3.0.1'
 
 
 from .util.db import create_engine
