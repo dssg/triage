316b3430a4015d108ac6a509bb50f6a257988bb7
Tristan Crockett <tristan.h.crockett@gmail.com>
2018-11-16 08:24:34 -0600

    Cohort Generation respects replace flag [Resolves #503]
    - Experiment passes replace flag to CohortTableGenerator
    - CohortTableGenerator, when replace=False, only runs cohort query for a date if it has no
    rows present.

--- src/triage/component/architect/cohort_table_generators.py
+++ src/triage/component/architect/cohort_table_generators.py
@@ -1,7 +1,7 @@
 import logging
 
 from triage.component.architect.database_reflection import table_has_data
-from triage.database_reflection import table_row_count
+from triage.database_reflection import table_row_count, table_exists
 
 
 DEFAULT_ACTIVE_STATE = "active"
@@ -21,11 +21,16 @@ class CohortTableGenerator(object):
         experiment_hash (string) unique identifier for the experiment
         query (string) SQL query string to select entities for a given as_of_date
             The as_of_date should be parameterized with brackets: {as_of_date}
+        replace (boolean) Whether or not to overwrite old rows.
+            If false, each as-of-date will query to see if there are existing rows
+                and not run the query if so.
+            If true, the existing cohort table will be dropped and recreated.
     """
-    def __init__(self, query, db_engine, cohort_table_name):
+    def __init__(self, query, db_engine, cohort_table_name, replace=True):
         self.db_engine = db_engine
         self.query = query
         self.cohort_table_name = cohort_table_name
+        self.replace = replace
 
     def generate_cohort_table(self, as_of_dates):
         """Convert the object's input table
@@ -54,6 +59,22 @@ class CohortTableGenerator(object):
             table_row_count(self.cohort_table_name, self.db_engine),
         )
 
+    def _maybe_create_cohort_table(self):
+        if self.replace or not table_exists(self.cohort_table_name, self.db_engine):
+            self.db_engine.execute(f"drop table if exists {self.cohort_table_name}")
+            self.db_engine.execute(
+                f"""create table {self.cohort_table_name} (
+                    entity_id integer,
+                    as_of_date timestamp,
+                    {DEFAULT_ACTIVE_STATE} boolean
+                )
+                """
+            )
+            logging.info("Created cohort table")
+        else:
+            logging.info("Not dropping and recreating cohort table because "
+                         "replace flag was set to False and table was found to exist")
+
     def _create_and_populate_cohort_table(self, as_of_dates):
         """Create a cohort table by sequentially running a
             given date-parameterized query for all known dates.
@@ -61,26 +82,27 @@ class CohortTableGenerator(object):
         Args:
         as_of_dates (list of datetime.date): Dates to calculate entity states as of
         """
-
-        self.db_engine.execute(
-            f"""create table {self.cohort_table_name} (
-                entity_id integer,
-                as_of_date timestamp,
-                {DEFAULT_ACTIVE_STATE} boolean
-            )
-            """
-        )
-        logging.info("Created cohort state table, now inserting rows")
-
+        self._maybe_create_cohort_table()
+        logging.info("Inserting rows into cohort table")
         for as_of_date in as_of_dates:
             formatted_date = f"{as_of_date.isoformat()}"
+            logging.info("Looking for existing cohort rows for as of date %s", as_of_date)
+            any_existing = list(self.db_engine.execute(
+                f"""select 1 from {self.cohort_table_name}
+                where as_of_date = '{formatted_date}'
+                limit 1
+                """
+            ))
+            if len(any_existing) == 1:
+                logging.info("Since >0 cohort rows found for date %s, skipping", as_of_date)
+                continue
             dated_query = self.query.format(as_of_date=formatted_date)
             full_query = f"""insert into {self.cohort_table_name}
                 select q.entity_id, '{formatted_date}'::timestamp, true
                 from ({dated_query}) q
                 group by 1, 2, 3
             """
-            logging.info("Running state query for date: %s, %s", as_of_date, full_query)
+            logging.info("Running cohort query for date: %s, %s", as_of_date, full_query)
             self.db_engine.execute(full_query)
 
     def _empty_table_message(self, as_of_dates):
