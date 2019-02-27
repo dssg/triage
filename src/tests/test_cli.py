9a3dfc9040113edcba761f37524381667099423e
Tristan Crockett <tristan.h.crockett@gmail.com>
2019-02-27 17:32:01 -0600

    Add database url patch to CLI test

--- src/tests/test_cli.py
+++ src/tests/test_cli.py
@@ -1,8 +1,11 @@
 import argcmdr
 import triage.cli as cli
 from unittest.mock import Mock, patch
+import os
 
 
+# we do not need a real database URL but one SQLalchemy thinks looks like a real one
+@patch.dict(os.environ, {'DATABASE_URL': 'postgresql://postgres@8.8.8.8/test'})
 def try_command(*argv):
     try:
         argcmdr.main(cli.Triage, argv=('--tb',) + argv)
