372d9c855686af66e373aa5a3af82f6028901516
tweddielin <tweddielin@gmail.com>
2019-02-28 11:52:35 -0600

    add docstrings

--- src/triage/component/risklist.py
+++ src/triage/component/risklist.py
@@ -11,9 +11,15 @@ import json
 import re
 
 
-def generate_risk_list(db_engine, matrix_storage_engine, model_storage_engine, model_id, as_of_date):
-    upgrade_db(db_engine=db_engine)
-    # 1. get feature and cohort config from database
+def get_required_info_from_config(db_engine, model_id):
+    """Get all information needed to make the risk list from model_id
+    Args:
+            db_engine (sqlalchemy.db.engine)
+            model_id (int) The id of a given model in the database
+
+    Returns: (dict) a dictionary of all information needed for making the risk list
+
+    """
     get_experiment_query = """
         select experiments.config, matrices.matrix_metadata, matrix_uuid
         from model_metadata.experiments
@@ -33,26 +39,51 @@ def generate_risk_list(db_engine, matrix_storage_engine, model_storage_engine, m
     timechop_config = experiment_config['temporal_config']
     feature_start_time = timechop_config['feature_start_time']
 
-    cohort_table_name = f"production.cohort_{cohort_config['name']}"
+    model_info = {}
+    model_info['cohort_config'] = cohort_config
+    model_info['feature_config'] = feature_config
+    model_info['feature_names'] = feature_names
+    model_info['feature_start_time'] = feature_start_time
+    model_info['original_matrix_uuid'] = original_matrix_uuid
+    model_info['label_config'] = label_config
+
+    return model_info
+
+
+def generate_risk_list(db_engine, matrix_storage_engine, model_storage_engine, model_id, as_of_date):
+    """Generate the risk list based model_id and as_of_date
+
+    Args:
+            db_engine (sqlalchemy.db.engine)
+            model_id (int) The id of a given model in the database
+            matrix_storage_engine (catwalk.storage.matrix_storage_engine)
+            model_storage_engine (catwalk.storage.model_storage_engine)
+
+    """
+    upgrade_db(db_engine=db_engine)
+    # 1. Get feature and cohort config from database
+    model_info = get_required_info_from_config(db_engine, model_id)
+
+    # 2. Generate cohort
+    cohort_table_name = f"production.cohort_{model_info['cohort_config']['name']}"
     cohort_table_generator = CohortTableGenerator(
         db_engine=db_engine,
-        query=cohort_config['query'],
+        query=model_info['cohort_config']['query'],
         cohort_table_name=cohort_table_name
     )
+    cohort_table_generator.generate_cohort_table([dt_from_str(as_of_date)])
 
+    # 3. Generate feature aggregations
     feature_generator = FeatureGenerator(
         db_engine=db_engine,
         features_schema_name="production",
-        feature_start_time=feature_start_time,
+        feature_start_time=model_info['feature_start_time'],
     )
-
-    cohort_table_generator.generate_cohort_table([dt_from_str(as_of_date)])
     collate_aggregations = feature_generator.aggregations(
-        feature_aggregation_config=feature_config,
+        feature_aggregation_config=model_info['feature_config'],
         feature_dates=[as_of_date],
         state_table=cohort_table_name
     )
-
     feature_generator.process_table_tasks(
         feature_generator.generate_all_table_tasks(
             collate_aggregations,
@@ -60,26 +91,24 @@ def generate_risk_list(db_engine, matrix_storage_engine, model_storage_engine, m
         )
     )
 
+    # 4. Reconstruct feature disctionary from feature_names and generate imputation
     reconstructed_feature_dictionary = {}
     imputation_table_tasks = OrderedDict()
     with db_engine.begin() as conn:
         for aggregation in collate_aggregations:
             feature_prefix = aggregation.prefix
-            feature_group = aggregation.get_table_name(imputed=True).split('.')[1]
-            feature_group = feature_group.replace('"', '')
-            feature_names_in_group = [f for f in feature_names if re.match(f'\A{feature_prefix}', f)]
+            feature_group = aggregation.get_table_name(imputed=True).split('.')[1].replace('"', '')
+            feature_names_in_group = [f for f in model_info['feature_names'] if re.match(f'\A{feature_prefix}', f)]
             reconstructed_feature_dictionary[feature_group] = feature_names_in_group
 
-            feature_names_in_group = set(feature_names_in_group)
-            features_imputed_in_train = set(f for f in feature_names_in_group if f + '_imp' in feature_names_in_group)
-
+            # Make sure that the features imputed in training should also be imputed in production
+            features_imputed_in_train = [f for f in set(feature_names_in_group) if f + '_imp' in feature_names_in_group]
             results = conn.execute(aggregation.find_nulls())
             null_counts = results.first().items()
 
-            features_imputed_in_production = set([col for (col, val) in null_counts if val > 0])
-
-            total_impute_cols = features_imputed_in_production | features_imputed_in_train
-            total_nonimpute_cols = set(f for f in feature_names_in_group if '_imp' not in f) - total_impute_cols
+            features_imputed_in_production = [col for (col, val) in null_counts if val > 0]
+            total_impute_cols = set(features_imputed_in_production) | set(features_imputed_in_train)
+            total_nonimpute_cols = set(f for f in set(feature_names_in_group) if '_imp' not in f) - total_impute_cols
             task_generator = feature_generator._generate_imp_table_tasks_for
             imputation_table_tasks.update(task_generator(
                 aggregation,
@@ -87,9 +116,9 @@ def generate_risk_list(db_engine, matrix_storage_engine, model_storage_engine, m
                 nonimpute_cols=list(total_nonimpute_cols)
                 )
             )
-
     feature_generator.process_table_tasks(imputation_table_tasks)
 
+    # 5. Build matrix
     db_config = {
         "features_schema_name": "production",
         "labels_schema_name": "public",
@@ -111,16 +140,16 @@ def generate_risk_list(db_engine, matrix_storage_engine, model_storage_engine, m
         'test_duration': '1y',
         'matrix_type': 'production',
         'label_timespan': None,
-        'label_name': label_config['name'],
+        'label_name': model_info['label_config']['name'],
         'indices': ["entity_id", "as_of_date"],
-        'feature_start_time': feature_start_time,
+        'feature_start_time': model_info['feature_start_time'],
     }
 
     matrix_uuid = metta.generate_uuid(matrix_metadata)
 
     matrix_builder.build_matrix(
         as_of_times=[as_of_date],
-        label_name=label_config['name'],
+        label_name=model_info['label_config']['name'],
         label_type=None,
         feature_dictionary=reconstructed_feature_dictionary,
         matrix_metadata=matrix_metadata,
@@ -128,6 +157,7 @@ def generate_risk_list(db_engine, matrix_storage_engine, model_storage_engine, m
         matrix_type="production",
     )
 
+    # 6. Predict the risk score for production
     predictor = Predictor(
         model_storage_engine=model_storage_engine,
         db_engine=db_engine
@@ -137,6 +167,5 @@ def generate_risk_list(db_engine, matrix_storage_engine, model_storage_engine, m
         model_id=model_id,
         matrix_store=matrix_storage_engine.get_store(matrix_uuid),
         misc_db_parameters={},
-        train_matrix_columns=matrix_storage_engine.get_store(original_matrix_uuid).columns()
+        train_matrix_columns=matrix_storage_engine.get_store(model_info['original_matrix_uuid']).columns()
     )
-
