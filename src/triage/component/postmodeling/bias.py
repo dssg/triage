ee75a94ba8deb56783c491656da13cc8419b11e5
Pedro Saleiro <pedrosaleiro@gmail.com>
2019-04-08 13:46:02 -0500

    adding bias audit to cli

--- src/triage/component/postmodeling/bias.py
+++ src/triage/component/postmodeling/bias.py
@@ -1,3 +1,4 @@
+import logging
 import pandas as pd
 import ohio.ext.pandas  # noqa
 
@@ -27,27 +28,29 @@ class AequitasConfigLoader(object):
 
 
 
-def aequitas_audit(df, configs, model_id=1, preprocessed=False):
+def aequitas_audit(df, configs, model_id, preprocessed=False):
     """
 
-    :param df:
-    :param ref_groups_method:
-    :param model_id:
-    :param configs:
-    :param report:
-    :param preprocessed:
-    :return:
+    Args:
+        df:
+        configs:
+        model_id:
+        preprocessed:
+
+    Returns:
+
     """
+
     if not preprocessed:
         df, attr_cols_input = preprocess_input_df(df)
         if not configs.attr_cols:
             configs.attr_cols = attr_cols_input
     g = Group()
-    print('Welcome to Aequitas-Audit')
-    print('Fairness measures requested:', ','.join(configs.fair_measures_requested))
+    logging.info('Welcome to Aequitas-Audit')
+    logging.info('Fairness measures requested:', ','.join(configs.fair_measures_requested))
     groups_model, attr_cols = g.get_crosstabs(df, score_thresholds=configs.score_thresholds, model_id=model_id,
                                               attr_cols=configs.attr_cols)
-    print('audit: df shape from the crosstabs:', groups_model.shape)
+    logging.info('audit: df shape from the crosstabs:', groups_model.shape)
     b = Bias()
     # todo move this to the new configs object / the attr_cols now are passed through the configs object...
     ref_groups_method = configs.ref_groups_method
@@ -57,36 +60,56 @@ def aequitas_audit(df, configs, model_id=1, preprocessed=False):
         bias_df = b.get_disparity_major_group(groups_model, df)
     else:
         bias_df = b.get_disparity_min_metric(groups_model, df)
-    print('Any NaN?: ', bias_df.isnull().values.any())
-    print('bias_df shape:', bias_df.shape)
+    logging.info('Any NaN?: ', bias_df.isnull().values.any())
+    logging.info('bias_df shape:', bias_df.shape)
 
 
     f = Fairness(tau=configs.fairness_threshold)
-    print('Fairness Threshold:', configs.fairness_threshold)
-    print('Fairness Measures:', configs.fair_measures_requested)
+    logging.info('Fairness Threshold:', configs.fairness_threshold)
+    logging.info('Fairness Measures:', configs.fair_measures_requested)
     group_value_df = f.get_group_value_fairness(bias_df, fair_measures_requested=configs.fair_measures_requested)
     return group_value_df
 
 
 
 
-def get_models_list(configs, engine):
+def get_models_list(config, engine):
+    """
+
+    Args:
+        config:
+        engine:
+
+    Returns:
+
+    """
     models_query = "select model_id from model_metadata.models"
-    if configs['model_group_id'] or configs['train_end_time']:
+    if config['model_group_id'] or config['train_end_time']:
         models_query += " where "
-        if configs['model_group_id']:
+        if config['model_group_id']:
             models_query += " model_group_id in ({model_group_id_list}) ".format(
-                    model_group_id_list = ",".join(configs['model_group_id']))
-        if configs['train_end_time']:
-            if configs['model_group_id']:
+                    model_group_id_list = ",".join(config['model_group_id']))
+        if config['train_end_time']:
+            if config['model_group_id']:
                 models_query += " and "
             models_query += " train_end_time in ({train_end_time_list}) ".format(
-                    train_end_time_list = ",".join(["'" + str(train_date) + "'::date " for train_date in configs['train_end_time']]))
+                    train_end_time_list = ",".join(["'" + str(train_date) + "'::date " for train_date in config['train_end_time']]))
     models_query += " order by model_id asc"
-    models_list = pd.DataFrame.pg_copy_from(models_query, engine)['model_id'].tolist()
-    return models_list
+    return pd.DataFrame.pg_copy_from(models_query, engine)['model_id'].tolist()
+
 
 def get_predictions(model_id, predictions_schema, config, engine):
+    """
+
+    Args:
+        model_id:
+        predictions_schema:
+        config:
+        engine:
+
+    Returns:
+
+    """
     # the entity_id (and as_of_date if wanted) comes from the attributes_query
     predictions_query = """ with attributes as ({attributes_query}) 
                         select p.model_id, p.score, a.*
@@ -101,43 +124,37 @@ def get_predictions(model_id, predictions_schema, config, engine):
     return pd.DataFrame.pg_copy_from(predictions_query, engine)
 
 
-def manage_audits(df, configs, preprocessed=False):
+def run_bias(engine, config, predictions_schemas=['test_results', 'train_results']):
+    """
 
-    group_value_df = None
-    if df is not None:
-        if 'model_id' in df.columns:
-            model_df_list = []
-            for model_id in df.model_id.unique():
-                model_df = audit(df.loc[df['model_id'] == model_id], model_id=model_id, configs=configs,
-                                               preprocessed=preprocessed)
-                model_df_list.append(model_df)
-            group_value_df = pd.concat(model_df_list)
+    Args:
+        engine:
+        config:
+        predictions_schemas:
 
-        else:
-            group_value_df, report = audit(df, configs=configs, preprocessed=preprocessed)
-    else:
-        logging.error('run_csv: could not load a proper dataframe from the input filepath provided.')
-    # print(report)
-    return group_value_df
+    Returns:
 
-
-def run_bias(engine, config):
-
-    create_tables = 'append'
-    if args.create_tables:
-        create_tables = 'replace'
-    input_query = config.db['input_query']
-
-    df = pd.DataFrame.pg_copy_from(input_query, engine)
-
-    group_value_df = run(df, configs=config, preprocessed=False)
-    try:
-        group_value_df.set_index(['model_id', 'attribute_name']).pg_copy_to(
-            schema=output_schema,
-            name=output_table,
-            con=engine,
-            if_exists=create_tables)
-    except SQLAlchemyError as e:
-        logging.error('push_db_data: Could not push results to the target database.' + e)
-
+    """
+    models_list = get_models_list(config)
+    if not models_list:
+        raise Exception("""
+            Postmodeling bias audit: get_models_list() returned empty model ids list.""")
+    for model_id in models_list:
+        for schema in predictions_schemas:
+            logging.info("Running aequitas audit for model_id={model_id} and predictions_schema={schema}".format())
+            input_df = get_predictions(model_id, schema, config, engine)
+            if not input_df:
+                raise Exception("""
+                Postmodeling bias audit: get_predictions() returned empty dataframe for model_id = {model_id}. 
+                and predictions_schema = {schema}""".format())
+            model_audit_df = aequitas_audit(input_df, model_id=model_id, configs=config, preprocessed=False)
+            if not model_audit_df:
+                raise Exception("""
+                Postmodeling bias audit: aequitas_audit() failed. Returned empty dataframe for model_id = {model_id}. 
+                and predictions_schema = {schema}""".format())
+            model_audit_df.set_index(['model_id', 'attribute_name']).pg_copy_to(
+                schema=schema,
+                name='bias_audit',
+                con=engine,
+                if_exists=config['replace_flag'])
 
