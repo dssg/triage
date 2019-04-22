6a46528a60562bc5f6798bd13eec6e4d8e4a3d49
Tristan Crockett <tristan.h.crockett@gmail.com>
2019-04-22 13:27:23 -0500

    Fix Travis deploy [Resolves #493]
    - Fixes the travis pypi deploy
    - Adds a travis github pages deploy and a manage command to run the
    local documentation site

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 import argparse
 from pathlib import Path
 
-from argcmdr import local, LocalRoot
+from argcmdr import local, LocalRoot, Local
+from plumbum import local as plumlocal
 
 
 ROOT_PATH = Path(__file__).parent.resolve()
@@ -15,7 +16,8 @@ class Development(LocalRoot):
 @Development.register
 @local('remainder', metavar='alembic arguments', nargs=argparse.REMAINDER)
 def alembic(context, args):
-    """Configuration wrapper to use the Alembic schema migrations library for Triage development"""
+    """Configuration wrapper to use the Alembic schema migrations library for Triage development.
+    Try alembic -h to see description of all available subcommands"""
     return context.local['env'][
         'PYTHONPATH=' + str(ROOT_PATH / 'src'),
         'alembic',
@@ -23,3 +25,11 @@ def alembic(context, args):
         '-x', 'db_config_file=database.yaml',
         args.remainder,
     ]
+
+
+@Development.register
+class Docs(Local):
+    """View Triage documentation through local server"""
+    def prepare(self, args):
+        with plumlocal.cwd(plumlocal.cwd / 'docs'):
+            yield plumlocal['mkdocs']['serve']
