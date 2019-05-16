c26edeaa9fc944afbbe01b40b08b61e4f008e63e
Tristan Crockett <tristan.h.crockett@gmail.com>
2019-05-16 11:52:43 -0500

    Update src/tests/results_tests/test_upgrade_if_clean.py
    Co-Authored-By: Jesse London <jesselondon@gmail.com>

--- src/tests/results_tests/test_upgrade_if_clean.py
+++ src/tests/results_tests/test_upgrade_if_clean.py
@@ -5,7 +5,7 @@ import pytest
 
 def test_upgrade_if_clean_upgrades_if_clean(db_engine):
     results_schema.upgrade_if_clean(db_engine.url)
-    db_version = list(db_engine.execute("select version_num from results_schema_versions"))[0][0]
+    db_version = db_engine.execute("select version_num from results_schema_versions").scalar()
     alembic_cfg = results_schema.alembic_config(db_engine.url)
     assert db_version == script.ScriptDirectory.from_config(alembic_cfg).get_current_head()
 
