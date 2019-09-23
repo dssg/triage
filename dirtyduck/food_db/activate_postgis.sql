22adff54d08c37fdbc855608a527393cfa50a8bf
Adolfo De Unánue <nanounanue@gmail.com>
2019-09-23 14:59:21 -0500

    Some extensions

--- dirtyduck/food_db/activate_postgis.sql
+++ dirtyduck/food_db/activate_postgis.sql
@@ -11,3 +11,23 @@ CREATE EXTENSION postgis_topology;
 CREATE EXTENSION postgis_sfcgal;
 
 CREATE EXTENSION pgrouting;
+
+
+create extension if not exists fuzzystrmatch;
+create extension if not exists unaccent;
+create extension if not exists pg_trgm;
+
+
+create extension if not exists bloom;
+
+create extension if not exists citext;
+
+create extension if not exists cube;
+
+create extension if not exists file_fdw;
+create extension if not exists postgres_fdw;
+
+create extension if not exists earthdistance;
+
+
+
