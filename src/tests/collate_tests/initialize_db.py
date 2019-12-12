5e59b9de7073de1a9692da0cf3ceb419256e7f4d
Kit Rodolfa <shaycrk@gmail.com>
2019-12-12 01:49:02 -0500

    remove debug print statements

--- src/tests/collate_tests/initialize_db.py
+++ src/tests/collate_tests/initialize_db.py
@@ -17,10 +17,6 @@ def handler(database):
 
 
 def load_data(connection):
-    #######
-#    print(connection.engine.url)
-#    print(DATA_PATH)
-    #######
     connection.execute("DROP TABLE IF EXISTS food_inspections")
     subprocess.run(
         [
@@ -35,8 +31,6 @@ def load_data(connection):
             str(DATA_PATH),
         ],
         check=True,
-#        stdout=subprocess.PIPE,   ###
-#        stderr=subprocess.PIPE,   ###
     )
     connection.execute("CREATE INDEX ON food_inspections(license_no, inspection_date)")
 
