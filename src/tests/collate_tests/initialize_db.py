aa2b139d47f5130edfa3f5444f06f72348f75a4a
Kit Rodolfa <shaycrk@gmail.com>
2019-12-12 00:11:26 -0500

    test error debugging

--- src/tests/collate_tests/initialize_db.py
+++ src/tests/collate_tests/initialize_db.py
@@ -34,7 +34,7 @@ def load_data(connection):
             str(DATA_PATH),
         ],
         check=True,
-        stdout=subprocess.PIPE,   ###
+#        stdout=subprocess.PIPE,   ###
         stderr=subprocess.PIPE,   ###
     )
     connection.execute("CREATE INDEX ON food_inspections(license_no, inspection_date)")
