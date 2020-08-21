43ff22c4f055f59fbe9fb16046b290e6a64fc7ec
Adolfo De Unánue <adolfo+git@unanue.mx>
2020-08-21 15:03:31 -0500

    30 partitions!

--- src/triage/component/results_schema/sql/predictions_hash_partitioning.sql
+++ src/triage/component/results_schema/sql/predictions_hash_partitioning.sql
@@ -21,27 +21,39 @@ create table test_results.predictions (
   test_label_timespan interval
 )   partition by hash(model_id);
 
-create table test_results.predictions_1 partition of test_results.predictions for values with (modulus 20, remainder 0);
-create table test_results.predictions_2 partition of test_results.predictions for values with (modulus 20, remainder 1);
-create table test_results.predictions_3 partition of test_results.predictions for values with (modulus 20, remainder 2);
-create table test_results.predictions_4 partition of test_results.predictions for values with (modulus 20, remainder 3);
-create table test_results.predictions_5 partition of test_results.predictions for values with (modulus 20, remainder 4);
-create table test_results.predictions_6 partition of test_results.predictions for values with (modulus 20, remainder 5);
-create table test_results.predictions_7 partition of test_results.predictions for values with (modulus 20, remainder 6);
-create table test_results.predictions_8 partition of test_results.predictions for values with (modulus 20, remainder 7);
-create table test_results.predictions_9 partition of test_results.predictions for values with (modulus 20, remainder 8);
-create table test_results.predictions_10 partition of test_results.predictions for values with (modulus 20, remainder 9);
+create table test_results.predictions_1 partition of test_results.predictions for values with (modulus 30, remainder 0);
+create table test_results.predictions_2 partition of test_results.predictions for values with (modulus 30, remainder 1);
+create table test_results.predictions_3 partition of test_results.predictions for values with (modulus 30, remainder 2);
+create table test_results.predictions_4 partition of test_results.predictions for values with (modulus 30, remainder 3);
+create table test_results.predictions_5 partition of test_results.predictions for values with (modulus 30, remainder 4);
+create table test_results.predictions_6 partition of test_results.predictions for values with (modulus 30, remainder 5);
+create table test_results.predictions_7 partition of test_results.predictions for values with (modulus 30, remainder 6);
+create table test_results.predictions_8 partition of test_results.predictions for values with (modulus 30, remainder 7);
+create table test_results.predictions_9 partition of test_results.predictions for values with (modulus 30, remainder 8);
+create table test_results.predictions_10 partition of test_results.predictions for values with (modulus 30, remainder 9);
 
-create table test_results.predictions_11 partition of test_results.predictions for values with (modulus 20, remainder 10);
-create table test_results.predictions_12 partition of test_results.predictions for values with (modulus 20, remainder 11);
-create table test_results.predictions_13 partition of test_results.predictions for values with (modulus 20, remainder 12);
-create table test_results.predictions_14 partition of test_results.predictions for values with (modulus 20, remainder 13);
-create table test_results.predictions_15 partition of test_results.predictions for values with (modulus 20, remainder 14);
-create table test_results.predictions_16 partition of test_results.predictions for values with (modulus 20, remainder 15);
-create table test_results.predictions_17 partition of test_results.predictions for values with (modulus 20, remainder 16);
-create table test_results.predictions_18 partition of test_results.predictions for values with (modulus 20, remainder 17);
-create table test_results.predictions_19 partition of test_results.predictions for values with (modulus 20, remainder 18);
-create table test_results.predictions_20 partition of test_results.predictions for values with (modulus 20, remainder 19);
+create table test_results.predictions_11 partition of test_results.predictions for values with (modulus 30, remainder 10);
+create table test_results.predictions_12 partition of test_results.predictions for values with (modulus 30, remainder 11);
+create table test_results.predictions_13 partition of test_results.predictions for values with (modulus 30, remainder 12);
+create table test_results.predictions_14 partition of test_results.predictions for values with (modulus 30, remainder 13);
+create table test_results.predictions_15 partition of test_results.predictions for values with (modulus 30, remainder 14);
+create table test_results.predictions_16 partition of test_results.predictions for values with (modulus 30, remainder 15);
+create table test_results.predictions_17 partition of test_results.predictions for values with (modulus 30, remainder 16);
+create table test_results.predictions_18 partition of test_results.predictions for values with (modulus 30, remainder 17);
+create table test_results.predictions_19 partition of test_results.predictions for values with (modulus 30, remainder 18);
+create table test_results.predictions_20 partition of test_results.predictions for values with (modulus 30, remainder 19);
+
+create table test_results.predictions_21 partition of test_results.predictions for values with (modulus 30, remainder 20);
+create table test_results.predictions_22 partition of test_results.predictions for values with (modulus 30, remainder 21);
+create table test_results.predictions_23 partition of test_results.predictions for values with (modulus 30, remainder 22);
+create table test_results.predictions_24 partition of test_results.predictions for values with (modulus 30, remainder 23);
+create table test_results.predictions_25 partition of test_results.predictions for values with (modulus 30, remainder 24);
+create table test_results.predictions_26 partition of test_results.predictions for values with (modulus 30, remainder 25);
+create table test_results.predictions_27 partition of test_results.predictions for values with (modulus 30, remainder 26);
+create table test_results.predictions_28 partition of test_results.predictions for values with (modulus 30, remainder 27);
+create table test_results.predictions_29 partition of test_results.predictions for values with (modulus 30, remainder 28);
+create table test_results.predictions_30 partition of test_results.predictions for values with (modulus 30, remainder 29);
+
 
 insert into test_results.predictions
 select model_id,
@@ -91,27 +103,39 @@ create table train_results.predictions (
 )   partition by hash(model_id);
 
 
-create table train_results.predictions_1 partition of train_results.predictions for values with (modulus 20, remainder 0);
-create table train_results.predictions_2 partition of train_results.predictions for values with (modulus 20, remainder 1);
-create table train_results.predictions_3 partition of train_results.predictions for values with (modulus 20, remainder 2);
-create table train_results.predictions_4 partition of train_results.predictions for values with (modulus 20, remainder 3);
-create table train_results.predictions_5 partition of train_results.predictions for values with (modulus 20, remainder 4);
-create table train_results.predictions_6 partition of train_results.predictions for values with (modulus 20, remainder 5);
-create table train_results.predictions_7 partition of train_results.predictions for values with (modulus 20, remainder 6);
-create table train_results.predictions_8 partition of train_results.predictions for values with (modulus 20, remainder 7);
-create table train_results.predictions_9 partition of train_results.predictions for values with (modulus 20, remainder 8);
-create table train_results.predictions_10 partition of train_results.predictions for values with (modulus 20, remainder 9);
+create table train_results.predictions_1 partition of train_results.predictions for values with (modulus 30, remainder 0);
+create table train_results.predictions_2 partition of train_results.predictions for values with (modulus 30, remainder 1);
+create table train_results.predictions_3 partition of train_results.predictions for values with (modulus 30, remainder 2);
+create table train_results.predictions_4 partition of train_results.predictions for values with (modulus 30, remainder 3);
+create table train_results.predictions_5 partition of train_results.predictions for values with (modulus 30, remainder 4);
+create table train_results.predictions_6 partition of train_results.predictions for values with (modulus 30, remainder 5);
+create table train_results.predictions_7 partition of train_results.predictions for values with (modulus 30, remainder 6);
+create table train_results.predictions_8 partition of train_results.predictions for values with (modulus 30, remainder 7);
+create table train_results.predictions_9 partition of train_results.predictions for values with (modulus 30, remainder 8);
+create table train_results.predictions_10 partition of train_results.predictions for values with (modulus 30, remainder 9);
 
-create table train_results.predictions_11 partition of train_results.predictions for values with (modulus 20, remainder 10);
-create table train_results.predictions_12 partition of train_results.predictions for values with (modulus 20, remainder 11);
-create table train_results.predictions_13 partition of train_results.predictions for values with (modulus 20, remainder 12);
-create table train_results.predictions_14 partition of train_results.predictions for values with (modulus 20, remainder 13);
-create table train_results.predictions_15 partition of train_results.predictions for values with (modulus 20, remainder 14);
-create table train_results.predictions_16 partition of train_results.predictions for values with (modulus 20, remainder 15);
-create table train_results.predictions_17 partition of train_results.predictions for values with (modulus 20, remainder 16);
-create table train_results.predictions_18 partition of train_results.predictions for values with (modulus 20, remainder 17);
-create table train_results.predictions_19 partition of train_results.predictions for values with (modulus 20, remainder 18);
-create table train_results.predictions_20 partition of train_results.predictions for values with (modulus 20, remainder 19);
+create table train_results.predictions_11 partition of train_results.predictions for values with (modulus 30, remainder 10);
+create table train_results.predictions_12 partition of train_results.predictions for values with (modulus 30, remainder 11);
+create table train_results.predictions_13 partition of train_results.predictions for values with (modulus 30, remainder 12);
+create table train_results.predictions_14 partition of train_results.predictions for values with (modulus 30, remainder 13);
+create table train_results.predictions_15 partition of train_results.predictions for values with (modulus 30, remainder 14);
+create table train_results.predictions_16 partition of train_results.predictions for values with (modulus 30, remainder 15);
+create table train_results.predictions_17 partition of train_results.predictions for values with (modulus 30, remainder 16);
+create table train_results.predictions_18 partition of train_results.predictions for values with (modulus 30, remainder 17);
+create table train_results.predictions_19 partition of train_results.predictions for values with (modulus 30, remainder 18);
+create table train_results.predictions_20 partition of train_results.predictions for values with (modulus 30, remainder 19);
+
+create table train_results.predictions_21 partition of train_results.predictions for values with (modulus 30, remainder 20);
+create table train_results.predictions_22 partition of train_results.predictions for values with (modulus 30, remainder 21);
+create table train_results.predictions_23 partition of train_results.predictions for values with (modulus 30, remainder 22);
+create table train_results.predictions_24 partition of train_results.predictions for values with (modulus 30, remainder 23);
+create table train_results.predictions_25 partition of train_results.predictions for values with (modulus 30, remainder 24);
+create table train_results.predictions_26 partition of train_results.predictions for values with (modulus 30, remainder 25);
+create table train_results.predictions_27 partition of train_results.predictions for values with (modulus 30, remainder 26);
+create table train_results.predictions_28 partition of train_results.predictions for values with (modulus 30, remainder 27);
+create table train_results.predictions_29 partition of train_results.predictions for values with (modulus 30, remainder 28);
+create table train_results.predictions_30 partition of train_results.predictions for values with (modulus 30, remainder 29);
+
 
 insert into train_results.predictions
 select model_id,
