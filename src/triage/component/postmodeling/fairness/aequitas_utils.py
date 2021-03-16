4e7a88848dc950ecc94b4b05e0f58b22877d7650
Evan Feder <evanmfeder@gmail.com>
2021-03-16 14:44:45 -0400

    Initial Aequitas utils, Notebook in dev to test

---
+++ src/triage/component/postmodeling/fairness/aequitas_utils.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def get_aequitas_results(engine, schema="test_results", table="aequtias", model_id=None):
+    ''' This function returns the current contents of the aequtias table.
+
+        Args:
+            - engine: SQLAlchemy engine conected to database
+            - schema: Databse schema to find table within
+            - table: Databse table to select data from
+            - model_id: A model_id, to query only for results of that model
+
+        Returns: A DataFrame, corresponding to schema.table
+    '''
+
+    query = f"SELECT * FROM {schema}.{table}"
+    if model_id:
+        query += f" WHERE model_id = {model_id}"
+    return pd.read_sql(query, con=engine)
