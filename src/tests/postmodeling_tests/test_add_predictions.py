d658baff43ae08b1da0ba8bacd3aa9f0029fbc7e
kasun <amarekas@gmail>
2021-06-04 15:20:44 +0000

    remove evaluators

--- src/tests/postmodeling_tests/test_add_predictions.py
+++ src/tests/postmodeling_tests/test_add_predictions.py
@@ -1,8 +1,24 @@
 import pytest
 
-from triage.component.postmodeling.contrast.model_group_evaluator import ModelGroupEvaluator
-from triage.component.postmodeling.contrast.model_evaluator import ModelEvaluator
 from triage.component.postmodeling.utils.predictions_selected_model_groups import generate_predictions
+
+MODEL_IDS_QUERY = """
+    SELECT
+        model_id
+    FROM triage_metadata.models
+    WHERE model_group_id={model_group_id}
+"""
+
+PREDICTIONS_QUERY = """
+    SELECT 
+        model_id,
+        entity_id,
+        as_of_date,
+        score
+    FROM test_results.predictions
+    WHERE model_id={model_id}
+"""
+
 
 def test_add_predictions(finished_experiment_without_predictions):
     db_engine = finished_experiment_without_predictions.db_engine
@@ -16,13 +32,10 @@ def test_add_predictions(finished_experiment_without_predictions):
     )
 
     for model_group_id in model_groups:
-        model_group_evaluator = ModelGroupEvaluator(model_group_id, db_engine)
-        model_ids = model_group_evaluator.model_id
+        model_ids = db_engine.execute(MODEL_IDS_QUERY.format(model_group_id))
 
         # Asserting that all model_ids in the groups have the predictions written to the DB
         for model_id in model_ids:
-            model_evaluator = ModelEvaluator(model_group_id, model_id, db_engine)
-            try:
-                model_evaluator.predictions
-            except RuntimeError as e:
-                pytest.fail('Predictions not saved RuntimeError -- {}'.format(e))
+            predictions = db_engine.execute(PREDICTIONS_QUERY.format(model_id))
+
+            assert len(predictions) > 0
