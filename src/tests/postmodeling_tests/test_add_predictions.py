c7b8934df482c2914ebc279398514d6c0975585e
kasun <amarekas@gmail>
2021-06-08 17:32:04 +0000

    test populating the table and presence of all model ids

--- src/tests/postmodeling_tests/test_add_predictions.py
+++ src/tests/postmodeling_tests/test_add_predictions.py
@@ -1,6 +1,8 @@
 import pytest
 
 from triage.component.postmodeling.utils.predictions_selected_model_groups import generate_predictions
+from triage.component.architect.database_reflection import table_has_data
+
 
 MODEL_IDS_QUERY = """
     SELECT
@@ -9,18 +11,16 @@ MODEL_IDS_QUERY = """
     WHERE model_group_id={model_group_id}
 """
 
-PREDICTIONS_QUERY = """
+MODELS_IN_PREDICTIONS_QUERY = """
     SELECT 
-        model_id,
-        entity_id,
-        as_of_date,
-        score
+        distinct model_id
     FROM test_results.predictions
-    WHERE model_id={model_id}
 """
 
 
-def test_add_predictions(finished_experiment_without_predictions):
+def test_populate_predictions_table(finished_experiment_without_predictions):
+    """assert that generate_predictions populate the predictions table"""
+
     db_engine = finished_experiment_without_predictions.db_engine
     model_groups = [1]
     project_path = finished_experiment_without_predictions.project_storage.project_path
@@ -31,20 +31,28 @@ def test_add_predictions(finished_experiment_without_predictions):
         project_path=project_path
     )
 
-    # We check for the number of predictions in the DB for each model in each model group
+    assert table_has_data('test_results.predictions', db_engine)
 
-    predictions_present = True
-    for model_group_id in model_groups:
-        model_ids = db_engine.execute(MODEL_IDS_QUERY.format(model_group_id))
 
-        # Asserting that all model_ids in the groups have the predictions written to the DB
-        for model_id in model_ids:
-            predictions = db_engine.execute(PREDICTIONS_QUERY.format(model_id))
-            if len(predictions) == 0:
-                predictions_present = False
-                break
+def test_add_predictions_all_models(finished_experiment_without_predictions):
+    """assert that generate_predictions write predictions of all models in the model group"""
+
+    db_engine = finished_experiment_without_predictions.db_engine
+    model_groups = [1]
+    project_path = finished_experiment_without_predictions.project_storage.project_path
+
+    generate_predictions(
+        db_engine=db_engine,
+        model_groups=model_groups,
+        project_path=project_path
+    )
+
+    # Model ids belonging to the model group 1
+    model_ids = db_engine.execute(MODEL_IDS_QUERY.format(model_group_id=1)).fetchall()
+    model_ids = {x[0] for x in model_ids}
+
+    # model ids present in the predictions table
+    model_ids_predictions = db_engine.execute(MODELS_IN_PREDICTIONS_QUERY).fetchall()
+    model_ids_predictions = {x[0] for x in model_ids_predictions}
         
-        if not predictions_present:
-            break
-            
-    assert predictions_present == True
+    assert model_ids == model_ids_predictions
