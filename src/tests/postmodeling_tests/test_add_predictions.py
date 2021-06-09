703f4f30df504fe135deabe98728ab766ffa56c6
kasun <amarekas@gmail>
2021-06-09 04:23:22 +0000

    test raising exceptions

--- src/tests/postmodeling_tests/test_add_predictions.py
+++ src/tests/postmodeling_tests/test_add_predictions.py
@@ -1,3 +1,5 @@
+import pytest
+
 from triage.component.postmodeling.utils.add_predictions import add_predictions
 from triage.component.architect.database_reflection import table_has_data
 
@@ -54,3 +56,88 @@ def test_add_predictions_all_models(finished_experiment_without_predictions):
     model_ids_predictions = {x[0] for x in model_ids_predictions}
         
     assert model_ids == model_ids_predictions
+
+
+def test_models_invalid_model_group(finished_experiment_without_predictions):
+    """Test whether the module produces the error for invalid model groups"""
+
+    db_engine = finished_experiment_without_predictions.db_engine
+    model_groups = [325]
+    project_path = finished_experiment_without_predictions.project_storage.project_path
+
+    with pytest.raises(ValueError):
+        add_predictions(
+            db_engine=db_engine,
+            model_groups=model_groups,
+            project_path=project_path
+        )
+
+
+def test_models_not_found_invalid_time_range(finished_experiment_without_predictions):
+    """Test whether the module errors when an invalid time range is provided"""
+    db_engine = finished_experiment_without_predictions.db_engine
+    model_groups = [1]
+    project_path = finished_experiment_without_predictions.project_storage.project_path
+
+    q = """
+        SELECT 
+            to_char(min(train_end_time) - '1month'::interval, 'YYYY-MM-DD') as lower_lim,
+            to_char(max(train_end_time) + '1month'::interval, 'YYYY-MM-DD') as upper_lim
+        FROM triage_metadata.models
+        where model_group_id = 1
+
+    """
+
+    end_times = db_engine.execute(q).fetchall()[0]
+    lower_lim = end_times[0]
+    upper_lim = end_times[1]
+
+    with pytest.raises(ValueError):
+        add_predictions(
+            db_engine=db_engine,
+            model_groups=model_groups,
+            project_path=project_path,
+            train_end_times_range={
+                'range_start_date': upper_lim,
+            }
+        )
+
+    with pytest.raises(ValueError):
+        add_predictions(
+            db_engine=db_engine,
+            model_groups=model_groups,
+            project_path=project_path,
+            train_end_times_range={
+                'range_end_date': lower_lim
+            }
+        )
+
+    with pytest.raises(ValueError):
+        add_predictions(
+            db_engine=db_engine,
+            model_groups=model_groups,
+            project_path=project_path,
+            train_end_times_range={
+                'range_start_date': upper_lim,
+                'range_end_date': upper_lim
+            }
+        )
+
+
+def test_invalid_experiment_hash(finished_experiment_without_predictions):
+    """Test whether an invalid experiment hash throws a ValueError"""
+
+    db_engine = finished_experiment_without_predictions.db_engine
+    model_groups = [1]
+    project_path = finished_experiment_without_predictions.project_storage.project_path
+
+    experiment_hash = finished_experiment_without_predictions.experiment_hash
+    experiment_hash = experiment_hash + 'x'
+
+    with pytest.raises(ValueError):
+        add_predictions(
+            db_engine=db_engine,
+            model_groups=model_groups,
+            project_path=project_path,
+            experiment_hashes=[experiment_hash]
+        )
