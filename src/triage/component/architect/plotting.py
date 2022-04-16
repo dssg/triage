c9ac1905125632c31d7ad24f2a68a9fe12f4c2b7
Tristan Crockett <tristan.h.crockett@gmail.com>
2022-04-15 23:44:04 -0500

    Squash to one function

--- src/triage/component/architect/plotting.py
+++ src/triage/component/architect/plotting.py
@@ -8,17 +8,17 @@ logger = verboselogs.VerboseLogger(__name__)
 import pydantic
 
 class CohortInspectionResults(pydantic.BaseModel):
-    ran_successfully: Optional[bool]
+    ran_successfully: bool
     num_rows: int
     distinct_entity_ids: int
     examples: List[str]
 
 def inspect_cohort_query_on_date(query: str, db_engine, as_of_date: datetime.date):
-    temp_table_name = 'temp_inspect_cohort'
+    cohort_table_name = 'temp_inspect_cohort'
     generator = EntityDateTableGenerator(
         query=query,
         db_engine=db_engine,
-        entity_date_table_name=temp_table_name,
+        entity_date_table_name=cohort_table_name,
         replace=True
     )
     results = {}
@@ -26,16 +26,6 @@ def inspect_cohort_query_on_date(query: str, db_engine, as_of_date: datetime.dat
     generator.generate_entity_date_table([as_of_date])
     logger.info('Cohort query successfully ran')
     results['ran_successfully'] = True
-    results.update(inspect_cohort_on_date(temp_table_name, as_of_date, db_engine))
-    return CohortInspectionResults(**results)
-
-
-def inspect_cohort_on_date(cohort_table_name, as_of_date, db_engine):
-    """Inspects a finished cohort
-
-    expects the table at cohort_table_name to be populated
-    """
-    results = {}
     results['num_rows'] = list(db_engine.execute(
         f'select count(*) from {cohort_table_name} where as_of_date = %s',
         as_of_date))[0][0]
@@ -47,6 +37,4 @@ def inspect_cohort_on_date(cohort_table_name, as_of_date, db_engine):
         row[0]
         for row in db_engine.execute(f'select entity_id from {cohort_table_name} ORDER BY random() limit 5')
     ])
-    
-
-    return results
+    return CohortInspectionResults(**results)
