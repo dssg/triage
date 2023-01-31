3b4a22c295deb0e2996af1569784d217cd33b6e4
Liliana Millan <liliana.millan@gmail.com>
2023-01-31 14:58:17 +0000

    error cols

--- src/triage/component/postmodeling/error_analysis.py
+++ src/triage/component/postmodeling/error_analysis.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
-def retrieve_scores_labels(model_id, db_conn):
+from src.triage.component.postmodeling.utils import _fetch_relevant_model_matrix_info
+from src.triage.component.catwalk.utils import sort_predictions_and_labels
+from src.triage.component.catwalk.storage import ProjectStorage
+
+
+def fetch_scores_labels(model_id, db_conn):
     """
-    Retrieves the scores and labels associated to a particular model id.
+    Given a model id, it retrieves its scores and labels.
     
     Args: 
         model_id (int): The model id from which scores and labels are going to be retrieved from. 
@@ -11,23 +16,30 @@ def retrieve_scores_labels(model_id, db_conn):
     Returns: 
         DataFrame with scores and labels for a specific model_id.
     """
-
+    # instead use _fetch_relevant_model_matrix_info from str/triage/components/postmodeling/utils/add_predictions.py
+    matrix_info = _fetch_relevant_model_matrix_info(db_conn, model_groups, experiment_hashes)
     q = """
-        --select
-        --    model_id,
-        --    entity_id, 
-        --    as_of_date,
-        --    score,
-        --    label_value
-        --from train_results.predictions
-        --where model_id = {model_id}
-        --union all
-        SELECT 
+        select
            model_id,
            entity_id, 
+           'train' as type,
            as_of_date,
            score,
-           label_value
+           label_value,
+           rank_abs_no_ties,
+           matrix_uuid
+        from train_results.predictions
+        where model_id = {model_id}
+        union all
+        SELECT 
+           model_id,
+           entity_id,
+           'test' as type,
+           as_of_date,
+           score,
+           label_value,
+           rank_abs_no_ties,
+           matrix_uuid
         FROM test_results.predictions
         WHERE model_id = {model_id}
     """.format(model_id=model_id)
@@ -46,31 +58,51 @@ def generate_error_labels(predictions, k):
         k (int): Number of resources to use
     
     Returns: 
-        DataFrame with three new columns associated to the labels for general error analysis, 
-        error analysis for recall and error analysis for precison.
+        DataFrame with two new columns associated to the labels generated for each type of error: general error, 
+        errors in positive label, errors in negative labels.
     """
 
-    # sort the scores desc 
-    sorted_scores = predictions.sort_values(by="score", ascending=False)
+    # sort the scores desc
+    sorted_scores = predictions.sort_values(by="rank_abs_no_ties")
     # add prediction column
     sorted_scores['prediction'] = '0'
-    sorted_scores.prediction.iloc[:k] = '1'
+    sorted_scores[sorted_scores.rank_abs_no_ties <= k] = 1
     # add type of label: TP, TN, FP, FN
     sorted_scores['type_label'] = 'TP'
-    sorted_scores.type_label.mask(~(sorted_scores.label) & (sorted_scores.prediction == '1'), 'FP', inplace=True)
-    sorted_scores.type_label.mask((sorted_scores.label) & (sorted_scores.prediction == '0'), 'FN', inplace=True)
-    sorted_scores.type_label.mask(~(sorted_scores.label) & (sorted_scores.prediction == '0'), 'TN', inplace=True)
+    sorted_scores.type_label.mask(~(sorted_scores.label_value) & (sorted_scores.prediction == '1'), 'FP', inplace=True)
+    sorted_scores.type_label.mask((sorted_scores.label_value) & (sorted_scores.prediction == '0'), 'FN', inplace=True)
+    sorted_scores.type_label.mask(~(sorted_scores.label_value) & (sorted_scores.prediction == '0'), 'TN', inplace=True)
     
     # add three new columns with error analysis labels
-    sorted_scores['error_label_recall'] = '0'
-    sorted_scores.error_label_recall.mask(sorted_scores.type_label == 'FN', '1', inplace=True)
-    sorted_scores['error_label_precision'] = '0'
-    sorted_scores.error_label_precision.mask(sorted_scores.type_label == 'FP', '1', inplace=True)
-    sorted_scores['error_label_general'] = '0'
-    sorted_scores.error_label_general.mask((sorted_scores.type_label == 'FP') | 
+    sorted_scores['error_negative_label'] = '0'
+    sorted_scores.error_negative_label.mask(sorted_scores.type_label == 'FN', '1', inplace=True)
+    sorted_scores['error_positive_label'] = '0'
+    sorted_scores.error_positive_label.mask(sorted_scores.type_label == 'FP', '1', inplace=True)
+    sorted_scores['error_general'] = '0'
+    sorted_scores.error_general.mask((sorted_scores.type_label == 'FP') | 
                                        (sorted_scores.type_label == 'FN'), '1', inplace=True)
-        
-    return sorted_scores
+    # melt columns to create error_type feature to use in DT
+    df = sorted_scores.melt(id_vars=['model_id', 'entity_id', 'type', 'as_of_date', 'score', 'label_value',
+    'rank_abs_no_ties', 'matrix_uuid', 'prediction', 'type_label'], value_vars=['error_negative_label', 
+    'error_positive_label', 'error_general'], var_name="error_type", value_name="error_label")
+
+    return df
+
+
+def fetch_matrices(model_id, project_path):
+   #_fetch_relevant_model_matrix_info()
+    project_storage = ProjectStorage(project_path)
+    matrix_storage_engine = project_storage.matrix_storage_engine()
+
+    matrix_store = matrix_storage_engine.get_store(matrix_uuid=matrix_uuid)
+    matrix = matrix_store.design_matrix
+    labels = matrix_store.labels
+    features = matrix.columns
+
+    # joining the predictions to the model
+    matrix = predictions.join(matrix, how='left')
+
+    pass
 
 
 def retrieve_features_matrix(model_id, db_conn, path, storage="s3"):
