d6b9266d2a1ab999fa1bccb295e19474e2947774
kasun <amarekas@gmail.com>
2023-02-07 19:51:08 +0000

    metadata, predictions, and tentative name

--- src/triage/component/postmodeling/contrast/model_class.py
+++ src/triage/component/postmodeling/contrast/model_class.py
@@ -1,7 +1,11 @@
+
+import pandas as pd
+
+
 from descriptors import cachedproperty
 
 
-class ModelSomething:
+class ModelAnalyzer:
 
     def __init__(self, model_id, engine):
         self.model_id=model_id
@@ -10,9 +14,9 @@ class ModelSomething:
     @cachedproperty
     def metadata(self):
         return next(self.engine.execute(
-                    f'''WITH
-                    individual_model_ids_metadata AS(
-                    SELECT m.model_id,
+                    f'''
+                    WITH individual_model_ids_metadata AS(
+                        SELECT m.model_id,
                            m.model_group_id,
                            m.hyperparameters,
                            m.model_hash,
@@ -24,11 +28,10 @@ class ModelSomething:
                         FROM triage_metadata.models m
                         JOIN triage_metadata.model_groups mg
                         USING (model_group_id)
-                        WHERE model_group_id = {self.model_group_id}
                         AND model_id = {self.model_id}
                     ),
                     individual_model_id_matrices AS(
-                    SELECT DISTINCT ON (matrix_uuid)
+                        SELECT DISTINCT ON (matrix_uuid)
                            model_id,
                            matrix_uuid,
                            evaluation_start_time as as_of_date
@@ -41,9 +44,14 @@ class ModelSomething:
                     SELECT metadata.*, test.*
                     FROM individual_model_ids_metadata AS metadata
                     LEFT JOIN individual_model_id_matrices AS test
-                    USING(model_id);''')
+                    USING(model_id);'''
+            )
         )
     
+
+    @property
+    def model_group_id(self):
+        return self.metadata['model_group_id']
 
     @property
     def model_type(self):
@@ -61,6 +69,7 @@ class ModelSomething:
     def train_matrix_uuid(self):
         return self.metadata['train_matrix_uuid']
 
+    # TODO: Need to figure out how this would work when there are multiple matrices in the evaluations table
     @property
     def pred_matrix_uuid(self):
         return self.metadata['matrix_uuid']
@@ -69,10 +78,52 @@ class ModelSomething:
     def as_of_date(self):
         return self.metadata['as_of_date']
 
+    @property
+    def train_end_time(self):
+        return self.metadata['train_end_time']
 
-    def get_predictions(matrix_uuid=None):
-        """Fetch the predictions from the DB for a given matrix"""
-        pass
+    @property
+    def train_label_timespan(self):
+        return self.metadata['training_label_timespan']
+
+    def get_predictions(self, matrix_uuid=None, fetch_null_labels=True):
+        """Fetch the predictions from the DB for a given matrix
+        
+            args:
+                matrix_uuid (optional):  
+        
+        """
+        where_clause = f"WHERE model_id = {self.model_id}"
+
+        if matrix_uuid is not None:
+            where_clause += f" AND matrix_uuid={matrix_uuid}"
+
+        if not fetch_null_labels:
+            where_clause += f" AND label_value IS NOT NULL"
+
+        query = f"""
+            SELECT model_id,
+                   entity_id,
+                   as_of_date,
+                   score,
+                   label_value,
+                   COALESCE(rank_abs_with_ties, RANK() OVER(ORDER BY score DESC)) AS rank_abs,
+                   COALESCE(rank_pct_with_ties, percent_rank()
+                   OVER(ORDER BY score DESC)) * 100 as rank_pct,
+                   test_label_timespan
+            FROM test_results.predictions
+            {where_clause}        
+        """
+
+        preds = pd.read_sql(query, self.engine)
+
+        #TODO: Maybe we should call the script to save predictions here?
+        if preds.empty:
+            raise RuntimeError(
+                "No predictions were found in the database. Please run the add_predictions module to add predictions for the model"
+            )
+
+        return preds
 
 
     def get_evaluations(parameter, threshold, tiebreaker_ordering):
