fa0fef5abe2a080f436300b3bb828c5ea7714ff1
kasun <amarekas@gmail.com>
2023-02-08 21:22:36 +0000

    crosstabs

--- src/triage/component/postmodeling/contrast/model_class.py
+++ src/triage/component/postmodeling/contrast/model_class.py
@@ -1,12 +1,20 @@
-
+import ohio.ext.pandas
 import pandas as pd
 import logging
 
+# remove this
+import psycopg2
 
 from descriptors import cachedproperty
 
+from sqlalchemy import create_engine
+
+from triage.component.catwalk.storage import ProjectStorage
+
 
 class ModelAnalyzer:
+
+    id_columns = ['entity_id', 'as_of_date']
 
     def __init__(self, model_id, engine):
         self.model_id=model_id
@@ -29,7 +37,7 @@ class ModelAnalyzer:
                         FROM triage_metadata.models m
                         JOIN triage_metadata.model_groups mg
                         USING (model_group_id)
-                        AND model_id = {self.model_id}
+                        WHERE model_id = {self.model_id}
                     ),
                     individual_model_id_matrices AS(
                         SELECT DISTINCT ON (matrix_uuid)
@@ -96,7 +104,7 @@ class ModelAnalyzer:
         where_clause = f"WHERE model_id = {self.model_id}"
 
         if matrix_uuid is not None:
-            where_clause += f" AND matrix_uuid={matrix_uuid}"
+            where_clause += f" AND matrix_uuid='{matrix_uuid}'"
 
         if not fetch_null_labels:
             where_clause += f" AND label_value IS NOT NULL"
@@ -107,9 +115,10 @@ class ModelAnalyzer:
                    as_of_date,
                    score,
                    label_value,
-                   COALESCE(rank_abs_with_ties, RANK() OVER(ORDER BY score DESC)) AS rank_abs,
-                   COALESCE(rank_pct_with_ties, percent_rank()
-                   OVER(ORDER BY score DESC)) * 100 as rank_pct,
+                   rank_abs_with_ties,
+                   rank_pct_with_ties,
+                   rank_abs_no_ties,
+                   rank_pct_no_ties, 
                    test_label_timespan
             FROM test_results.predictions
             {where_clause}        
@@ -123,7 +132,7 @@ class ModelAnalyzer:
                 "No predictions were found in the database. Please run the add_predictions module to add predictions for the model"
             )
 
-        return preds
+        return preds.set_index(self.id_columns)
 
 
     def get_evaluations(self, metrics=None, matrix_uuid=None, subset_hash=None):
@@ -144,7 +153,7 @@ class ModelAnalyzer:
         where_clause = f'WHERE model_id={self.model_id}'
 
         if matrix_uuid is not None:
-            where_clause += f" AND matrix_uuid={matrix_uuid}"
+            where_clause += f" AND matrix_uuid='{matrix_uuid}'"
 
         if subset_hash is not None:
             where_clause += f" AND subset_hash={subset_hash}"
@@ -178,6 +187,114 @@ class ModelAnalyzer:
 
         return evaluations
 
+    def crosstabs_pos_vs_neg(self, project_path, thresholds, matrix_uuid=None, push_to_db=True, table_name='crosstabs', return_df=True):
+        """ Generate crosstabs for the predicted positives (top-k) vs the rest
+    
+        args:
+            project_path (str): Path where the experiment artifacts (models and matrices) are stored
+            thresholds (Dict{str: Union[float, int}]): A dictionary that maps threhold type to the threshold
+                                                    The threshold type can be one of the rank columns in the test_results.predictions_table
+            return_df (bool, optional): Whether to return the constructed df or just to store in the database
+                                        Defaults to False (only writing to the db)
+            
+            matrix_uuid (str, optional): To run crosstabs for a different matrix than the validation matrix from the experiment
 
-    def get_crosstabs(threshold_type, tiebreaker_ordering, matix_uuid=None):
-        pass
+            push_to_db (bool, optional): Whether to write the results to the database. Defaults to True
+            table_name (str, optional): Table name to use in the db's `test_results` schema. Defaults to crosstabs.
+                                        If the table exists, results are appended
+
+            return_df (bool, optional): Whether to return the crosstabs as a dataframe. Defaults to True
+        """
+
+
+        positive_n = lambda hr, lr: pd.Series(index=hr.columns, data=hr.shape[0])
+        negative_n = lambda hr, lr: pd.Series(index=lr.columns, data=lr.shape[0])
+        positive_mean = lambda hr, lr: hr.mean(axis=0)
+        negative_mean = lambda hr, lr: lr.mean(axis=0)
+        positive_std = lambda hr, lr: hr.std(axis=0)
+        negative_std = lambda hr, lr: lr.std(axis=0)
+        ratio_positive_negative = lambda hr, lr: hr.mean(axis=0) / lr.mean(axis=0)
+        positive_support = lambda hr, lr: (hr > 0).sum(axis=0)
+        negative_support = lambda hr, lr: (lr > 0).sum(axis=0)
+
+
+        crosstab_functions = [
+            # The count features seem redundant, and is present in the evaluations table if needed
+            # ("count_predicted_positive", positive_n),
+            # ("count_predicted_negative", negative_n),
+            ("mean_predicted_positive", positive_mean),
+            ("mean_predicted_negative", negative_mean),
+            ("std_predicted_positive", positive_std),
+            ("std_predicted_negative", negative_std),
+            ("mean_ratio_predicted_positive_to_predicted_negative", ratio_positive_negative),
+            ("support_predicted_positive", positive_support),
+            ("support_predicted_negative", negative_support),
+        ]
+
+
+        if matrix_uuid is None:
+            matrix_uuid = self.pred_matrix_uuid
+
+        predictions = self.get_predictions(matrix_uuid=matrix_uuid)
+
+    
+        if predictions.empty:
+            logging.error(f'No predictions found for {self.model_id} and {matrix_uuid}. Exiting!')
+            raise ValueError(f'No predictions found {self.model_id} and {matrix_uuid}')
+
+
+        # initializing the storage engines
+        project_storage = ProjectStorage(project_path)
+        matrix_storage_engine = project_storage.matrix_storage_engine()
+
+        matrix_store = matrix_storage_engine.get_store(matrix_uuid=matrix_uuid)
+
+        matrix = matrix_store.design_matrix
+        
+        labels = matrix_store.labels
+        features = matrix.columns
+
+        # joining the predictions to the model
+        matrix = predictions.join(matrix, how='left')
+
+        all_results = list() 
+        
+        for threshold_name, threshold in thresholds.items():
+            logging.info(f'Crosstabs using threshold: {threshold_name} <= {threshold}')
+
+            msk = matrix[threshold_name] <= threshold
+            postive_preds = matrix[msk][features]
+            negative_preds = matrix[~msk][features]
+
+            temp_results = list()
+            for name, func in crosstab_functions:
+                logging.info(name)
+
+                this_result = pd.DataFrame(func(postive_preds, negative_preds))
+                this_result['metric'] = name
+                temp_results.append(this_result)
+            
+            temp_results = pd.concat(temp_results)
+            temp_results['threshold_type'] = threshold_name
+            temp_results['threshold'] = threshold
+
+            all_results.append(temp_results)
+
+        crosstabs_df = pd.concat(all_results).reset_index()
+
+        crosstabs_df.rename(columns={'index': 'feature', 0: 'value'}, inplace=True)
+        crosstabs_df['model_id'] = self.model_id
+        crosstabs_df['matrix_uuid'] = matrix_uuid
+
+    
+        if push_to_db:
+            logging.info('Pushing the results to the DB')
+            crosstabs_df.set_index(
+                ['model_id', 'matrix_uuid', 'feature', 'metric', 'threshold_type', 'threshold'], inplace=True
+            )
+
+            # TODO: Figure out to change the owner of the table
+            crosstabs_df.pg_copy_to(schema='test_results', name=table_name, con=self.engine, if_exists='append')
+
+        if return_df:
+            return crosstabs_df
