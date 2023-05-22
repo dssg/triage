7892ba0e7a402163f0c351d82c8ce52bb592112f
Liliana Millan <liliana.millan@gmail.com>
2023-05-22 16:26:49 +0000

    downcasting int64

--- src/triage/util/pandas.py
+++ src/triage/util/pandas.py
@@ -20,10 +20,20 @@ def downcast_matrix(df):
     logger.spam("Downcasting matrix.")
     logger.spam(f"Starting memory usage: {df.memory_usage(deep=True).sum()/1000000} MB")
     logger.spam(f"Initial types: \n {df.dtypes}")
-    new_df = df.apply(lambda x: x.astype(np.float32))
+    logger.spam(f"Changing int64 to int32 (if any)")
+    if df.select_dtypes("int64").shape[1] > 0: 
+        new_df_ints = df.select_dtypes("int64").apply(lambda x: x.astype(np.int32))
+    logger.spam("Changin float64 to float32 (if any)")
+    if df.select_dtypes("float64").shape[1] > 0: 
+        new_df_floats = df.select_dtypes("float64").apply(lambda x: x.astype(np.float32))
+    
+    new_df = pd.concat([new_df_ints, new_df_floats], axis=1)
 
     logger.spam("Downcasting matrix completed.")
     logger.spam(f"Final memory usage: {new_df.memory_usage(deep=True).sum()/1000000} MB")
     logger.spam(f"Final data types: \n {new_df.dtypes}")
+
+    # explicitly delete the previous df to reduce use of memory
+    del(df)
 
     return new_df
