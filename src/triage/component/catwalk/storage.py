eee00389c66ad23ceb62b999b1342b5e44fcb0c8
Liliana Millan <liliana.millan@gmail.com>
2023-05-23 21:06:19 +0000

    load csv by chunks

--- src/triage/component/catwalk/storage.py
+++ src/triage/component/catwalk/storage.py
@@ -614,7 +614,8 @@ class CSVMatrixStore(MatrixStore):
 
     def _load(self):
         with self.matrix_base_store.open("rb") as fd:
-            return pd.read_csv(fd, compression="gzip", parse_dates=["as_of_date"])
+            dfs = pd.read_csv(fd, compression="gzip", parse_dates=["as_of_date"], chunksize=1000)
+            return pd.concat(dfs)
     
     def load_csv(self, path_, matrix_uuid, suffix):
         with open(path_ + "/" + matrix_uuid + suffix, "rb") as fd:
