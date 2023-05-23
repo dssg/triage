8d6c5b930b4177b750fc6ff687d6d6a4b5e1bdd4
kasun <amarekas@gmail.com>
2023-05-23 16:49:46 +0000

    generalize plot grid creation, function to compare top-k lists, modify displaying model groups

--- src/triage/component/postmodeling/contrast/report_generator.py
+++ src/triage/component/postmodeling/contrast/report_generator.py
@@ -3,8 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import itertools
+
 
 from descriptors import cachedproperty
+from scipy.stats import spearmanr
 
 from triage.component.postmodeling.contrast.model_class import ModelAnalyzer
 
@@ -19,6 +22,9 @@ class PostmodelingReport:
         if use_all_model_groups: # shortcut to listing out all model groups for an experiment
             self.use_all_model_groups()
         self.models = self.get_model_ids()
+
+        #TODO: Would be good to let the user restrict the train_end_times included in the report
+
         
     @property
     def model_ids(self):
@@ -45,11 +51,23 @@ class PostmodelingReport:
             for train_end_time in self.models[mg]:
                 model_analyzer = self.models[mg][train_end_time]
                 data_dict.append([mg, train_end_time, model_analyzer.model_id, model_analyzer.model_type, model_analyzer.hyperparameters])
-        data_df = pd.DataFrame(data_dict, columns=['model_group_id', 'train_end_time', 'model_id', 'model_type', 'hyperparameters'])
-        return data_df
+        all_models = pd.DataFrame(data_dict, columns=['model_group_id', 'train_end_time', 'model_id', 'model_type', 'hyperparameters'])
+
+        # displaying the model_group_ids, model_type, and the hyperparameters
+        to_print = all_models.groupby('model_group_id').nth(1)[['model_type', 'hyperparameters']].reset_index().to_dict(orient='records')
+
+        for m in to_print:
+            print(m)
+
+        return all_models
 
     def get_model_ids(self):
-        """for the model group ids, fetch the model_ids and initialize the datastructure"""
+        """ For the model group ids, fetch the model_ids and initialize the datastructure
+
+            The data structure is a dictionary of dictionaries that maps the individual models of groups to their ModelAnalyzer class
+                {model_group_id: {train_end_time: ModelAnalyzer(model_id)}}
+                Here, the train_end_time is saved as a string of with format 'YYYY-MM-DD'
+        """
 
         model_groups = "', '".join([str(x) for x in self.model_groups])
         experiment_hashes = "', '".join(self.experiment_hashes)
@@ -76,19 +94,20 @@ class PostmodelingReport:
                 d[m['model_group_id']] = {m['train_end_time']: ModelAnalyzer(m['model_id'], self.engine)}
 
         return d 
-            
+    
     def _get_subplots(self, subplot_width=3, subplot_len=None, n_rows=None, n_cols=None):
         """"""
 
         if subplot_len is None:
             subplot_len = subplot_width
 
+        # num of rows == number of train_end_times
         if n_rows is None: 
-            n_rows = len(self.model_groups)
+            n_rows = len(self.models[self.model_groups[0]])
         
+        # num of cols == number of model groups
         if n_cols is None: 
-            n_cols = len(self.models[self.model_groups[0]])
-        
+            n_cols = len(self.model_groups)
         
         fig, axes = plt.subplots(
             n_rows,
@@ -100,85 +119,48 @@ class PostmodelingReport:
 
         return fig, axes
 
-    def plot_score_distributions(self, use_labels):
+
+    def _make_plot_grid(self, plot_type, subplot_width=3, subplot_len=None, **kw):
+        """
+            Abstracts out generating the plot grid (time x model group) for comparing
+            model_scores,  
+        """
+        fig, axes = self._get_subplots(subplot_width=subplot_width, subplot_len=subplot_len)
+
+        for j, mg in enumerate(self.models):
+            for i, train_end_time in enumerate(self.models[mg]):
+                model_analyzer = self.models[mg][train_end_time]
+
+                plot_func = getattr(model_analyzer, plot_type)
+
+                plot_func(ax=axes[i, j], **kw)
+
+                if j==0:
+                    axes[i, j].set_ylabel(f'{train_end_time}')
+                else:
+                    axes[i, j].set_ylabel('')
+                
+                axes[i, j].set_xlabel('')
+
+        fig.tight_layout()
+
+        return fig
+
+    def plot_score_distributions(self, use_labels=False):
         """for the model group ids plot score grid"""
 
-        # FIXME -- This breaks if only one model group is given to the report generator
-        fig, axes = self._get_subplots()
-
-        for i, mg in enumerate(self.models):
-            for j, train_end_time in enumerate(self.models[mg]):
-                mode_analyzer = self.models[mg][train_end_time]
-
-                if use_labels:
-                    mode_analyzer.plot_score_label_distribution(ax=axes[i, j])
-                else:
-                    mode_analyzer.plot_score_distribution(
-                        ax=axes[i, j]
-                    )
-
-                if j==0:
-                    axes[i, j].set_ylabel(f'Mod Grp: {mg}')
-                else:
-                    axes[i, j].set_ylabel('')
-                
-                if i == 0:
-                    axes[i, j].set_title(f'{train_end_time}({mode_analyzer.model_id})')
-                else:
-                    axes[i, j].set_title('')
-
-        fig.suptitle('Score Distributions')
-        fig.tight_layout()
+        if use_labels:
+            self._make_plot_grid(plot_type='plot_score_label_distribution')
+        else:
+            self._make_plot_grid(plot_type='plot_score_distribution')
         
-
     def plot_calibration_curves(self):
-        fig, axes = self._get_subplots()
-
-        for i, mg in enumerate(self.models):
-            for j, train_end_time in enumerate(self.models[mg]):
-                mode_analyzer = self.models[mg][train_end_time]
-
-                mode_analyzer.plot_calibration_curve(
-                    ax=axes[i, j]
-                )
-
-                if j==0:
-                    axes[i, j].set_ylabel(f'Mod Grp: {mg}')
-                else:
-                    axes[i, j].set_ylabel('')
-                
-                if i == 0:
-                    axes[i, j].set_title(f'{train_end_time} ({mode_analyzer.model_id})')
-                else:
-                    axes[i, j].set_title('')
-
-        fig.suptitle('Calibration Curves')
-        fig.tight_layout()
+        """calibration curves for all models"""
+        self._make_plot_grid(plot_type='plot_calibration_curve')
 
     def plot_prk_curves(self):
-        fig, axes = self._get_subplots()
-
-        for i, mg in enumerate(self.models):
-            for j, train_end_time in enumerate(self.models[mg]):
-                mode_analyzer = self.models[mg][train_end_time]
-
-                mode_analyzer.plot_precision_recall_curve(
-                    ax=axes[i, j]
-                )
-
-                if j==0:
-                    axes[i, j].set_ylabel(f'Mod Grp: {mg}')
-                else:
-                    axes[i, j].set_ylabel('')
-                
-                if i == 0:
-                    axes[i, j].set_title(f'{train_end_time} ({mode_analyzer.model_id})')
-                else:
-                    axes[i, j].set_title('')
-
-        fig.suptitle('Precision-Recall with Positive Prediction %')
-        fig.tight_layout()
-
+        self._make_plot_grid(plot_type='plot_precision_recall_curve')
+  
     def plot_bias_threshold(self, attribute_name, attribute_values, bias_metric):
         """
             Plot bias_metric for the specified list of attribute_values for a particular attribute_name across different thresholds (list %)
@@ -227,72 +209,11 @@ class PostmodelingReport:
 
     def plot_feature_importance(self, n_top_features=20):
         """ plot all feature importance  """
-
-        # For readability, we will plot the feature importance with time advancing on the vertical
-        fig, axes = self._get_subplots(
-            subplot_width=7,
-            n_rows = len(self.models[self.model_groups[0]]), # train end tomes
-            n_cols = len(self.model_groups) # mdoel groups
-        )
-
-        for j, mg in enumerate(self.models):
-            for i, train_end_time in enumerate(self.models[mg]):
-                mode_analyzer = self.models[mg][train_end_time]
-
-                mode_analyzer.plot_feature_importance(
-                    ax=axes[i, j],
-                    n_top_features=n_top_features
-                )
-
-                if j==0:
-                    axes[i, j].set_ylabel(f'{train_end_time}') # first column
-                else:
-                    axes[i, j].set_ylabel('')
-                
-                # if i == 0:
-                #     # axes[i, j].set_title(f'{train_end_time} ({mode_analyzer.model_id})')
-                #     axes[i, j].set_title(f'Mod Grp: {mg}') # Top row
-
-                # # else:
-                #     # axes[i, j].set_title('')
-
-        # fig.suptitle(f'{n_top_features} Features with highest importance (magnitude)')
-        fig.tight_layout()
-
+        self._make_plot_grid(plot_type='plot_feature_importance', subplot_width=7, n_top_features=n_top_features)
+        
     def plot_feature_group_importance(self, n_top_groups=20):
         """ plot all feature group importance  """
-
-        # For readability, we will plot the feature importance with time advancing on the vertical
-        fig, axes = self._get_subplots(
-            subplot_width=7,
-            n_rows = len(self.models[self.model_groups[0]]), # train end tomes
-            n_cols = len(self.model_groups) # mdoel groups
-        )
-
-        for j, mg in enumerate(self.models):
-            for i, train_end_time in enumerate(self.models[mg]):
-                mode_analyzer = self.models[mg][train_end_time]
-
-                mode_analyzer.plot_feature_group_importance(
-                    ax=axes[i, j],
-                    n_top_groups=n_top_groups
-                )
-
-                if j==0:
-                    axes[i, j].set_ylabel(f'{train_end_time} ({mode_analyzer.model_id})') # first column
-                else:
-                    axes[i, j].set_ylabel('')
-                
-                if i == 0:
-                    # axes[i, j].set_title(f'{train_end_time} ({mode_analyzer.model_id})')
-                    axes[i, j].set_title(f'Mod Grp: {mg}') # Top row
-
-                else:
-                    axes[i, j].set_title('')
-
-        fig.suptitle(f'Feature groups with highest importance (magnitude)')
-        fig.tight_layout()
-
+        self._make_plot_grid(plot_type='plot_feature_group_importance', subplot_width=7, n_top_groups=n_top_groups)
 
 
     def create_scatter_disparity_performance(self, metric, parameter, aeq_parameter, attr_col, attribute_values, 
@@ -385,7 +306,7 @@ class PostmodelingReport:
                     **kwargs
                 )
 
-        
+
     def display_crosstab_pos_vs_neg(
         self, 
         threshold_type,
@@ -458,14 +379,79 @@ class PostmodelingReport:
         if return_dfs:
             return dfs
 
+    def pairwise_list_comparison(self, threshold_type, threshold, train_end_time, matrix_uuid=None, plot=True):
+        """For a given train_end_time, compares the lists generated by the analyzed model groups
+
+        Args:
+            threshold_type (str): Type of the ranking to use. Has to be one of the four ranking types used in triage
+                    - rank_pct_no_ties 
+                    - rank_pct_with_ties
+                    - rank_abs_no_ties
+                    - rank_abs_with_ties
+            threshold (Union[float, int]): The threshold rank for creating the list. Int for 'rank_abs_*' and Float for 'rank_pct_*'
+            train_end_time (str): The prediction date we care about in YYYY-MM-DD format
+        """
+
+        lists = dict()
+
+        for mg, m in self.models.items():
+            lists[mg] = m[train_end_time].get_top_k(threshold_type, threshold, matrix_uuid)
+
         
+        pairs = list(itertools.combinations(lists.keys(), 2))
+
+        logging.info(f'Performing {len(pairs)} comparisons')
+
+        metrics = ['jaccard', 'overlap', 'rank_corr']
+        results = dict()
+
+        # Initializing three data frames to hold pairwise metrics
+        for m in metrics:
+            results[m] = pd.DataFrame(index=self.model_groups, columns=self.model_groups)
 
 
+        for model_group_pair in pairs:
+            logging.info(f'Comparing {model_group_pair[0]} and {model_group_pair[1]}')
 
+            df1 = lists[model_group_pair[0]]
+            df2 = lists[model_group_pair[1]]
 
-    
-    
+            # calculating jaccard similarity and overlap
+            entities_1 = set(df1.entity_id)
+            entities_2 = set(df2.entity_id)
 
+            inter = entities_1.intersection(entities_2)
+            un = entities_1.union(entities_2)    
+            results['jaccard'].loc[model_group_pair[1], model_group_pair[0]] = len(inter)/len(un)
+
+            # If the list sizes are not equal, using the smallest list size to calculate simple overlap
+            results['overlap'].loc[model_group_pair[1], model_group_pair[0]] = len(inter)/ min(len(entities_1), len(entities_2))
+
+            # calculating rank correlation
+            df1.sort_values('score', ascending=False, inplace=True)
+            df2.sort_values('score', ascending=False, inplace=True)
+
+            # only returning the corr coefficient, not the p-value
+            results['rank_corr'].loc[model_group_pair[0], model_group_pair[1]] = spearmanr(df1.entity_id.iloc[:], df2.entity_id.iloc[:])[0]
+
+        if plot:
+            fig, axes = plt.subplots(1, len(metrics), figsize=(10, 3))            
+            
+            for i, m in enumerate(metrics):
+                sns.heatmap(
+                    data=results[m].fillna(0),
+                    cmap='Greens',
+                    vmin=0,
+                    vmax=1,
+                    annot=True,
+                    linewidth=0.1,
+                    ax=axes[i]
+                )
+
+                axes[i].set_title(m)
+
+            fig.tight_layout()
+        return results
 
 
 
