fb904ba9441940b88a4433c5ab63387608b43319
Kasun <amarekas@gmail.com>
2025-06-26 21:46:17 +0000

    test crosstabs from matrix

--- src/tests/postmodeling_tests/test_crosstabs.py
+++ src/tests/postmodeling_tests/test_crosstabs.py
@@ -1,4 +1,6 @@
-import pytest 
+
+
+import pandas as pd
 
 from triage.component.postmodeling.crosstabs import run_crosstabs, run_crosstabs_from_matrix
 from triage.database_reflection import table_has_data
@@ -9,14 +11,12 @@ def test_run_crosstabs(finished_experiment, crosstabs_config):
     expected_table_name = (
         crosstabs_config.output["schema"] + "." + crosstabs_config.output["table"]
     )
-    table_has_data(expected_table_name, finished_experiment.db_engine)
+    assert table_has_data(expected_table_name, finished_experiment.db_engine)
 
-
+    
 def test_run_crosstabs_from_matrix(finished_experiment):
     """assert that crosstabs table is populated for all threshold types"""
-    project_path = finished_experiment.project_storage.project_path
-    engine=finished_experiment.db_engine
-    model_id = 1
+    
     thresholds = {
         'rank_abs_no_ties': 50,
         'rank_abs_with_ties': 50, 
@@ -26,17 +26,32 @@ def test_run_crosstabs_from_matrix(finished_experiment):
     table_schema='test_results'
     table_name = 'crosstabs'
     
-    for threshold_type, threshold in thresholds.items():
-        run_crosstabs_from_matrix(
-            db_engine=engine,
-            project_path=project_path,
-            model_id=model_id,
+    errors = list()
+    for threshold_type, threshold in thresholds.items():    
+        df = run_crosstabs_from_matrix(
+            db_engine=finished_experiment.db_engine,
+            project_path=finished_experiment.project_path,
+            model_id=1,
             threshold_type=threshold_type,
             threshold=threshold,
             push_to_db=True,
             table_schema=table_schema,
-            table_name=table_name
+            table_name=table_name,
+            replace=True
         )
         
-        assert table_has_data(f'{table_schema}.{table_name}', engine)
-    
+        q = f'''
+            select 
+            1
+            from {table_schema}.{table_name}
+            where model_id = 1
+            and threshold_type = '{threshold_type}'
+            and threshold = {threshold}
+        '''
+        
+        df = pd.read_sql(q,finished_experiment.db_engine)
+                
+        if df.empty:
+            errors.append(threshold_type)
+ 
+    assert not errors, f"errors occured for: {errors}"    
