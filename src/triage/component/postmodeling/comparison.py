98bb644bd7673326e0861f6b8c009a66bc1457dc
Kasun <amarekas@gmail.com>
2025-07-17 17:56:20 +0000

    list comparison heatmaps with altair

--- src/triage/component/postmodeling/comparison.py
+++ src/triage/component/postmodeling/comparison.py
@@ -3,6 +3,9 @@ import altair as alt
 import logging
 import seaborn as sns
 import matplotlib.pyplot as plt 
+import numpy as np
+
+import itertools
 
 import altair as alt
 
@@ -11,6 +14,8 @@ from .utils import (
     validation_group_model_exists
 ) 
 from triage.component.catwalk.evaluation import ModelEvaluator
+
+from triage.component.postmodeling.base import SingleModelAnalyzer
 
 class ModelGroupComparison:
     
@@ -281,6 +286,147 @@ class ModelGroupComparison:
                 )
                 
                 chart.display()
+
+    def topk_list(self, threshold_type, threshold):
+        """ Compare topk lists given a threshold      
+        """
+        
+        q = f'''
+            with topk_predictions as (
+                select 
+                    p.entity_id,
+                    m.model_id,
+                    m.model_group_id,
+                    m.train_end_time,
+                    (string_to_array(model_type, '.'))[
+                        array_length(string_to_array(model_type, '.'), 1) 
+                    ] as model_type
+                from test_results.predictions p 
+                left join triage_metadata.models m using(model_id)
+                where p.{threshold_type} <= {threshold}
+                and m.model_group_id in ({', '.join([str(x) for x in self.model_group_ids])})
+            ),
+            set_sizes as (
+                select 
+                train_end_time, 
+                model_id, 
+                model_group_id,
+                model_type,
+                count(distinct entity_id) as topk_size
+                from topk_predictions
+                group by 1, 2, 3, 4
+            ),
+            pairwise_overlap as (
+                select 
+                    a.train_end_time::date,
+                    a.model_group_id as model_group_1,
+                    a.model_id as model_id_1,
+                    a.model_type as model_type_1,
+                    b.model_group_id as model_group_2,
+                    b.model_id as model_id_2,
+                    b.model_type as model_type_2,
+                    count(distinct a.entity_id) as intersection_size
+                from topk_predictions a 
+                inner join topk_predictions b 
+                on a.entity_id = b.entity_id
+                and a.train_end_time = b.train_end_time
+                and a.model_group_id <= b.model_group_id
+                group by a.train_end_time, a.model_group_id, b.model_group_id, a.model_id, b.model_id, a.model_type, b.model_type
+                order by 1, 2, 4
+            )
+            select 
+                o.train_end_time::date,
+                o.model_group_1, 
+                o.model_group_2,
+                o.model_id_1,
+                o.model_id_2,
+                o.model_type_1,
+                o.model_type_2,
+                s1.topk_size as topk_size_1,
+                s2.topk_size as topk_size_2,
+                s1.topk_size + s2.topk_size - o.intersection_size as union_size,
+                o.intersection_size as overlap_abs,
+                o.intersection_size::numeric / least(s1.topk_size, s2.topk_size) as overlap,
+                o.intersection_size::numeric / 
+                    (nullif(s1.topk_size + s2.topk_size - o.intersection_size, 0)) as jaccard
+            from pairwise_overlap o join set_sizes s1
+            on s1.model_id = o.model_id_1
+                join set_sizes s2 
+                on s2.model_id = o.model_id_2
+        '''
+        
+        overlaps = pd.read_sql(q, self.engine)
+        overlaps["train_end_time"] = pd.to_datetime(overlaps["train_end_time"])
+        
+        # TODO: Rank Correlation
+        
+        # If there only two model groups, a line graph is drawn to indicate overlap and jaccard over time
+        if len(self.model_group_ids) == 2:
+            overlap_chart = alt.Chart(overlaps).mark_line().encode(
+                x='train_end_time',
+                y='overlap'
+            )
+            jaccard_chart = alt.Chart(overlaps).mark_line().encode(
+                x='train_end_time',
+                y='jaccard'
+            )
+
+        else:
+            # TODO: A lot of work todo here to make this code more nice. Currently very hacky
+            base = alt.Chart(overlaps).encode(
+                x=alt.X('model_group_1:N', axis=alt.Axis(title='')),
+                y=alt.Y('model_group_2:N', axis=alt.Axis(title='')),
+            ).properties(
+                width=80*len(self.model_group_ids),
+                height=80*len(self.model_group_ids),
+            )
+
+            overlap_agg = base.mark_rect().encode(
+                color=alt.Color('overlap', title='Top-k Overlap (%)', aggregate='mean'),
+                tooltip=[
+                    alt.Tooltip('overlap', title='overlap'),
+                    alt.Tooltip('model_type_1:N', title='X: '),
+                    alt.Tooltip('model_type_2:N', title='Y: ')
+                ]
+            ).properties(
+                title='Overlap of Top-k: Average over time'
+            )
+
+            overlap_agg_label = base.mark_text(color='black', fontSize=12).encode(
+                alt.Text('overlap', aggregate='mean', format='.3f')
+            )
+
+            overlapp_over_time = base.mark_rect().encode(
+                color=alt.Color('overlap', title='Top-k Overlap (%)', aggregate='mean')
+            )
+
+            overlap_label_over_time = base.mark_text(color='black', fontSize=12).encode(
+                alt.Text('overlap', format='.3f')
+            )
+
+            jaccard_agg = base.mark_rect().encode(
+                color=alt.Color('jaccard', title='Top-k Overlap (%)', aggregate='mean')
+            ).properties(
+                title='Jaccard Sim. of Top-k: Average over time'
+            )
+
+            jaccard_agg_label = base.mark_text(color='black').encode(
+                alt.Text('jaccard', aggregate='mean', format='.3f')
+            )
+
+            jaccard_over_time = base.mark_rect().encode(
+                color=alt.Color('jaccard', title='Top-k Overlap (%)', aggregate='mean')
+            )
+
+            jaccard_label_over_time = base.mark_text(color='black').encode(
+                alt.Text('jaccard', aggregate='mean', format='.3f')
+            )
+
+
+            overlap_chart = overlap_agg + overlap_agg_label | (overlapp_over_time + overlap_label_over_time).facet(column=alt.Column('train_end_time', title='', header=alt.Header(labelFontSize=13))).properties(title=alt.Title('Overlap by Validation Cohort', anchor='start'))
+            jaccard_chart = jaccard_agg + jaccard_agg_label | (jaccard_over_time + jaccard_label_over_time).facet(column=alt.Column('train_end_time', title='')).properties(title=alt.Title('Jaccard Similarity by Validation Cohort', anchor='start'))    
+    
+        return overlaps, overlap_chart, jaccard_chart
 
 
 class ModelComparisonError(ValueError):
